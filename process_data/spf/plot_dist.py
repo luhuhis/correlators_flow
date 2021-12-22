53ddfe98bb1cdaadd582706ab2ce7ff4f9cccb75
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2021-12-22 15:46:03 +0100

    adapt plotting scripts to changes in spf_reconstruct.py

--- process_data/spf/plot_dist.py
+++ process_data/spf/plot_dist.py
@@ -12,12 +12,14 @@ def main():
     requiredNamed.add_argument('--obs', help='which observable to plot', choices=["kappa", "chisqdof"], type=str, required=True)
 
     # identify the spf model
-    requiredNamed.add_argument('--model', help='which model to use', choices=[2], type=int, required=True)
-    requiredNamed.add_argument('--PhiUVtype', help='specify it this is LO (a) or NLO (b)', type=str, choices=["a", "b"], required=True)
-    requiredNamed.add_argument('--mu', help='which "en" function to use', choices=["alpha", "beta"], type=str, required=True)
-    requiredNamed.add_argument('--nmax', help='what nmax to use. valid only for model 1,2.', choices=[1, 2, 3, 4, 5, 6], type=int, required=True)
-    parser.add_argument('--constrain', help='force the spf to reach the UV limit at large omega', action="store_true")
-    requiredNamed.add_argument('--nsamples', help='number of bootstrap samples to draw from the gaussian distribution', default=200, type=int)
+    requiredNamed.add_argument('--model', help='input file identifier. which model was used', choices=[2], type=int, required=True)
+    requiredNamed.add_argument('--PhiUVtype', help='input file identifier. which PhiUV was used [ LO (a) or NLO (b) ]', type=str, choices=["a", "b"], required=True)
+    parser.add_argument('--constrain', help='input file identifier. whether the spf was forced to reach the UV limit at large omega', action="store_true")
+    requiredNamed.add_argument('--mu', help='input file identifier. which "en" function was used', choices=["alpha", "beta"], type=str, required=True)
+    requiredNamed.add_argument('--nmax', help='input file identifier. which nmax was used. valid only for model 1,2.', choices=[1, 2, 3, 4, 5, 6], type=int, required=True)
+    requiredNamed.add_argument('--nsamples', help='input file identifier. number of bootstrap samples that were used.', type=int)
+    requiredNamed.add_argument('--tol', help='input file identifier. tolerance that was used', type=float)
+    parser.add_argument('--start_from_mean_fit', help='input file identifier. whether the inital fit params where determined by a fit of the mean.', action="store_true")
     # parser.add_argument('--omega_idx', help='index which omega to plot', default=0, type=int)
 
     # plot args
@@ -25,11 +27,16 @@ def main():
 
     args = parser.parse_args()
 
-    constrainstr = "s1" if not args.constrain else "s2"
-    fileidentifier = str(args.model)+"_"+constrainstr+"_"+str(args.mu)+"_"+args.PhiUVtype+"_"+str(args.nmax)+"_"+str(args.nsamples)
+    constrainstr = "s1" if not args.constrain else "s2"  # s1 = dont constrain, s2 = constrain
+    startstr = "" if not args.start_from_mean_fit else "_m"
+    if args.model == 2:
+        modelidentifier = str(args.model) + "_" + args.PhiUVtype + "_" + constrainstr + "_" + str(args.mu) + "_" + str(args.nmax)
+    elif args.model == 3:
+        modelidentifier = str(args.model) + "_" + args.PhiUVtype
+    fileidentifier = modelidentifier + "_" + str(args.nsamples) + "_" + '{:.0e}'.format(args.tol) + startstr
 
     # read in the normalized correlator
-    inputfolder = "../"+lpd.get_merged_data_path(args.qcdtype, args.corr, "")+"/spf/"
+    inputfolder = "../"+lpd.get_merged_data_path(args.qcdtype, args.corr, "")+"/spf/"+fileidentifier+"/"
     outputfolder = inputfolder if not args.PathOutputFolder else args.PathOutputFolder
 
     index = None
@@ -46,7 +53,7 @@ def main():
         xlabel = r'$\chi^2/\mathrm{d.o.f.}$'
 
     samples = numpy.load(inputfolder+"samples_"+fileidentifier+".npy")
-    structure = numpy.loadtxt(inputfolder+"samples_structure_"+fileidentifier+".txt", dtype=int)
+    structure = numpy.loadtxt(inputfolder+"samples_structure_"+fileidentifier+".dat", dtype=int)
     ab = structure[index]
 
     samples = samples[:, ab[0]:ab[1]]
@@ -55,13 +62,13 @@ def main():
     # if args.obs == "spf":
     #     samples = samples[:, args.omega_idx]
 
-    fig, ax, plots = lpd.create_figure(ylabel=r'$n$', xlabel=xlabel, xlabelpos=(0.95, 0.2), ylabelpos=(0.02, 0.97), UseTex=False)
+    fig, ax, plots = lpd.create_figure(xlims=(0, 15), ylabel=r'$n$', xlabel=xlabel, xlabelpos=(0.95, 0.2), ylabelpos=(0.02, 0.97), UseTex=False)
 
     _, bins, _ = ax.hist(samples, args.nbins)
 
     ax.set_title(str(len(bins))+' bins, '+fileidentifier)
 
-    fig.savefig(outputfolder+"/"+args.corr+"_"+fileidentifier+"_"+args.obs+".pdf")
+    fig.savefig(outputfolder+"/"+args.corr+"_"+args.obs+"_"+fileidentifier+".pdf")
 
 
 if __name__ == '__main__':
--- process_data/spf/plot_kappas.py
+++ process_data/spf/plot_kappas.py
@@ -7,6 +7,7 @@ def main():
     parser, requiredNamed = lpd.get_parser()
 
     parser.add_argument('--PathOutputFolder', help='the path of output folder like /a/b', type=str)
+    # parser.add_argument('--oldformat', help='read in data files of the old format', action="store_true")
 
     # identify the run
     requiredNamed.add_argument('--nsamples', help='number of bootstrap samples', type=int, required=True)
@@ -17,26 +18,38 @@ def main():
     inputfolder = "../"+lpd.get_merged_data_path(args.qcdtype, args.corr, "")+"/spf/"
     outputfolder = inputfolder if not args.PathOutputFolder else args.PathOutputFolder
 
-    labels =      ("2_s2_alpha_a_4",    "2_s2_alpha_a_5",    "2_s1_alpha_a_4",   "2_s1_alpha_a_5",     "2_s2_beta_a_4",    "2_s2_beta_a_5",    "2_s1_beta_a_4",    "2_s1_beta_a_5")
+    labels =      ("2_a_s2_alpha_4_100_1e-04",    "2_s2_alpha_a_5",    "2_s1_alpha_a_4",   "2_s1_alpha_a_5",     "2_s2_beta_a_4",    "2_s2_beta_a_5",    "2_s1_beta_a_4",    "2_s1_beta_a_5")
     labels_plot = (r'$s_2 \alpha a 4$', r'$s_2 \alpha a 5$', r'$s_1 \alpha a 4$', r'$s_1 \alpha a 5$', r'$s_2 \beta a 4$', r'$s_2 \beta a 5$', r'$s_1 \beta a 4$', r'$s_1 \beta a 5$')
     kappas = []
+    errorsleft = []
+    errorsright = []
     for label in labels:
         try:
-            kappas.append(numpy.genfromtxt(inputfolder+"fitparams_chisqdof_"+label+"_"+str(args.nsamples)+".dat", skip_footer=1)[0:2, 0])
-        except:
-            print(label, " not found")
-            kappas.append([numpy.nan, numpy.nan])
+            file = inputfolder + "/"+label+"/"+"params_" + label + ".dat"
+            print("read in", file)
+            data = numpy.loadtxt(file)
+        except OSError:
+            print(label, "not found, skipping")
+            kappas.append(numpy.nan)
+            errorsleft.append(numpy.nan)
+            errorsright.append(numpy.nan)
+            continue
+        kappas.append(data[0, 0])
+        errorsleft.append(data[0, 1])
+        errorsright.append(data[0, 2])
+
     print(kappas)
+    print(errorsleft, errorsright)
 
     pos = 0.3+numpy.asarray((0.45, 0.85, 1.25, 1.65, 2.65, 3.05, 3.45, 3.85))
     colors = ('red', 'red', 'blue', 'blue', 'red', 'red', 'blue', 'blue')
 
     xlabel = r'$\kappa / T^3$'
 
-    fig, ax, plots = lpd.create_figure(xlabel=xlabel, xlabelpos=(0.5, -0.1), UseTex=False) #ylims=(0, 5), xlims=(0,5),
+    fig, ax, plots = lpd.create_figure(xlabel=xlabel, xlabelpos=(0.5, -0.1), ylims=(0, 5), UseTex=False)
 
     for i, label in enumerate(labels):
-        ax.errorbar(kappas[i][0], pos[i], xerr=kappas[i][1], color=colors[i], fmt='x-', fillstyle='none', markersize=5, mew=0.25, lw=0.8, elinewidth=0.5, capsize=1.2)
+        ax.errorbar(kappas[i], pos[i], xerr=[[errorsleft[i]], [errorsright[i]]], color=colors[i], fmt='x-', fillstyle='none', markersize=5, mew=0.25, lw=0.8, elinewidth=0.5, capsize=1.2)
 
     ax.set_yticks(pos)
     ax.set_yticklabels(labels_plot)
