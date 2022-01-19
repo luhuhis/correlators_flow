e21e9de6d8cf76ecf99df173e3ebb4ab508e0916
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-01-19 18:00:20 +0100

    add template script

---
+++ template.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3.7 -u
+import lib_process_data as lpd
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    lpd.print_script_call()
+    main()
+    lpd.save_script_call()
