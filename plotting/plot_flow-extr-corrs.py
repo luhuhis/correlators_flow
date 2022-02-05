98288ec55b29266c78abf5270f091566692c8a35
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-02-05 17:02:16 +0100

    add option to plot correlator at fixed flowradius/tauT

--- plotting/plot_flow-extr-corrs.py
+++ plotting/plot_flow-extr-corrs.py
@@ -39,6 +39,8 @@ def main():
     parser.add_argument('--c_b', help='prefactor of UV spf in model3a', required=True, type=float, default=1)
     parser.add_argument('--use_tex', action="store_true")
     parser.add_argument('--npoints', help='how many tauT to plot between 0 and 0.5 for the model correlators', default=100, type=int)
+    parser.add_argument('--ylims', help='custom ylims', nargs=2, type=float, default=(-0.03, 4))
+    parser.add_argument('--xlims', help='custom xlims', nargs=2, type=float, default=(-0.01, 0.51))
 
     args = parser.parse_args()
 
@@ -61,7 +63,7 @@ def main():
 
     MaxOmegaByT = PhiUVa[0][-1]
 
-    # spf = PhiUVa
+    # spfs
     PhiUVa_corr = []
     PhiUVb_corr = []
     PhiIRacorr = []
@@ -97,6 +99,107 @@ def main():
 
     basepath_plot = "/work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/EE/"
     fig.savefig(basepath_plot+"EE_flow_extr_comparison.pdf")
+
+    # ==========================================================================================================================================================
+
+    # /work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/
+    EE_cont_arr = []
+    flowradii = numpy.loadtxt("/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/s144t36_b0754400/flowradii_s144t36_b0754400.dat")
+    min_idx = 0
+    max_idx = 0
+    stop = False
+    for flowradius in flowradii:
+        flowradius_str = r'{0:.4f}'.format(flowradius)
+        file = "/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_" + flowradius_str + "_cont.txt"
+        try:
+            EE_cont_arr.append(numpy.loadtxt(file, unpack=True))
+            stop = True
+            max_idx += 1
+        except OSError:
+            # print("could not find ", file)
+            if not stop:
+                min_idx += 1
+    max_idx += min_idx
+    flowradii = flowradii[min_idx:max_idx]
+
+    fig2, ax2, _ = lpd.create_figure(xlims=args.xlims, ylims=args.ylims, xlabel=r'$\tau T$', xlabelpos=(0.95, 0.05), ylabelpos=(0.08, 0.9),
+                                     UseTex=args.use_tex,
+                                     ylabel=r'$\frac{G^*}{ G^\mathrm{norm} }$', figsize=(1.5 * (3 + 3 / 8), 1.5 * (3 + 3 / 8) / 16 * 9))
+
+    ax2.errorbar(XX[-1][0], XX[-1][1], XX[-1][2], zorder=-1000, **lpd.chmap(lpd.plotstyle_add_point, fmt='-', label=r'$G^{\tau_F\rightarrow 0}_\mathrm{cont}$'))
+
+    # find frankenstein correlator at fixed flowradiusBytauT
+    flowradiusBytauT_arr = (0.14, 0.2)
+    frankenstein_arr = []
+    for flowradiusBytauT in flowradiusBytauT_arr:
+        frankenstein_edata = []
+        frankenstein_ydata = []
+        frankenstein_xdata = []
+        for i, tauT in enumerate(EE_cont_arr[0][0]):
+            if tauT is not numpy.nan:
+                wanted_flowradius = flowradiusBytauT * tauT
+                index = (numpy.fabs(flowradii - wanted_flowradius)).argmin()
+                if index > 0:
+                    frankenstein_edata.append(EE_cont_arr[index][2][i])
+                    frankenstein_ydata.append(EE_cont_arr[index][1][i])
+                    frankenstein_xdata.append(tauT)
+        frankenstein_arr.append(numpy.column_stack((frankenstein_xdata, frankenstein_ydata, frankenstein_edata)))
+        numpy.savetxt("/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/frankenstein_"+r'{0:.3f}'.format(flowradiusBytauT)+".txt",
+                      numpy.column_stack((frankenstein_xdata, frankenstein_ydata, frankenstein_edata)),
+                      header="tauT      G/Gnorm_sqrt(8tauF)/tau="+r'{0:.3f}'.format(flowradiusBytauT)+"       err")
+        ax2.errorbar(frankenstein_xdata, frankenstein_ydata, frankenstein_edata, **lpd.chmap(lpd.plotstyle_add_point, markersize=0, fmt='x',
+                                                                                             label=r'$G_{\sqrt{8\tau_F}/\tau='+r'{0:.3f}'.format(flowradiusBytauT)+r'}$'))
+
+    ax2.errorbar(xpoints, model3bcorr, **lpd.chmap(lpd.plotstyle_add_point, fmt='--', zorder=-10, label=r'$G_\mathrm{\kappa/T^3=' + str(args.kappa_b) + r', c=' + str(
+        args.c_b) + r'}^{\mathrm{max(IR,c UV_{b})}}$'))
+    ax2.errorbar(xpoints, model3acorr, **lpd.chmap(lpd.plotstyle_add_point, fmt='--', zorder=-11, label=r'$G_\mathrm{\kappa/T^3=' + str(args.kappa_a) + r', c=' + str(
+        args.c_a) + r'}^{\mathrm{max(IR,c UV_{a})}}$'))
+    # ax2.errorbar(xpoints, PhiUVa_corr, **lpd.chmap(lpd.plotstyle_add_point, fmt='-', label=r'$G_{\mathrm{UV_a}}$'))
+    # ax2.errorbar(xpoints, PhiUVb_corr, **lpd.chmap(lpd.plotstyle_add_point, fmt='-', label=r'$G_{\mathrm{UV_b}}$'))
+    # ax2.errorbar(xpoints, PhiIRbcorr, **lpd.chmap(lpd.plotstyle_add_point, fmt='-.', label=r'$G_{\Phi_\mathrm{IR_b}}$'))
+    # ax2.errorbar(xpoints, PhiIRacorr, **lpd.chmap(lpd.plotstyle_add_point, fmt='-.', label=r'$G_{\Phi_\mathrm{IR_a}}$'))
+
+
+    lpd.legendstyle.update(dict(labelspacing=1, loc="lower right", bbox_to_anchor=(1.01, 0.07)))
+    ax2.legend(**lpd.chmap(lpd.legendstyle, bbox_to_anchor=(1, 0.5), loc='center left'))
+    ax2.set_title(r'quenched, $1.5 T_c$, IR=$\kappa \omega /2T$', x=0.5, y=1, fontsize=8)
+
+    fig2.savefig(basepath_plot + "EE_frankenstein_comparison.pdf")
+
+    # ==========================================================================================================================================================
+
+    fig3, ax3, _ = lpd.create_figure(xlims=args.xlims, xlabel=r'$\tau T$', xlabelpos=(0.95, 0.05), ylabelpos=(0.08, 0.9),
+                                     UseTex=args.use_tex,
+                                     ylabel=None, figsize=(1.5 * (3 + 3 / 8), 1.5 * (3 + 3 / 8) / 16 * 9))
+
+    for i, flowradiusBytauT in enumerate(flowradiusBytauT_arr):
+        # spfs
+        PhiUVa_corr = []
+        PhiUVb_corr = []
+        PhiIRacorr = []
+        PhiIRbcorr = []
+        model3acorr = []
+        model3bcorr = []
+        for x in frankenstein_arr[i][:, 0]:
+            PhiUVa_corr.append(args.c_a*scipy.integrate.quad(lambda OmegaByT: Integrand(OmegaByT, x, PhiUVaByT3), 0, MaxOmegaByT)[0] / Gnorm(x))
+            PhiUVb_corr.append(args.c_b*scipy.integrate.quad(lambda OmegaByT: Integrand(OmegaByT, x, PhiUVbByT3), 0, MaxOmegaByT)[0] / Gnorm(x))
+            PhiIRacorr.append(scipy.integrate.quad(lambda OmegaByT: Integrand(OmegaByT, x, PhiIR, args.kappa_a), 0, MaxOmegaByT)[0] / Gnorm(x))
+            PhiIRbcorr.append(scipy.integrate.quad(lambda OmegaByT: Integrand(OmegaByT, x, PhiIR, args.kappa_b), 0, MaxOmegaByT)[0] / Gnorm(x))
+            model3acorr.append(scipy.integrate.quad(lambda OmegaByT: Integrand(OmegaByT, x, model3, args.kappa_a, PhiUVaByT3, args.c_a), 0, MaxOmegaByT)[0] / Gnorm(x))
+            model3bcorr.append(scipy.integrate.quad(lambda OmegaByT: Integrand(OmegaByT, x, model3, args.kappa_b, PhiUVbByT3, args.c_b), 0, MaxOmegaByT)[0] / Gnorm(x))
+
+        ax3.errorbar(frankenstein_arr[i][:, 0], frankenstein_arr[i][:, 1]/PhiUVa_corr, frankenstein_arr[i][:, 2]/PhiUVa_corr,
+                     **lpd.chmap(lpd.plotstyle_add_point, fmt='-', label=r'$\frac{G_{\sqrt{8\tau_F}/\tau='+r'{0:.3f}'.format(flowradiusBytauT)+r'}}{ c\Phi_\mathrm{UV}^a}$'))
+
+        ax3.errorbar(frankenstein_arr[i][:, 0], frankenstein_arr[i][:, 1] / PhiUVb_corr, frankenstein_arr[i][:, 2] / PhiUVb_corr,
+                     **lpd.chmap(lpd.plotstyle_add_point, fmt='-',
+                                 label=r'$\frac{G_{\sqrt{8\tau_F}/\tau=' + r'{0:.3f}'.format(flowradiusBytauT) + r'}}{c \Phi_\mathrm{UV}^b}$'))
+
+    lpd.legendstyle.update(dict(loc="lower right", bbox_to_anchor=(1.01, 0.07)))
+    ax3.legend(**lpd.chmap(lpd.legendstyle, bbox_to_anchor=(1, 0.5), loc='center left'))
+    ax3.set_title(r'quenched, $1.5 T_c$', x=0.5, y=1, fontsize=8)
+
+    fig3.savefig(basepath_plot + "EE_frankenstein_comparison_ratio.pdf")
 
 
 if __name__ == '__main__':
