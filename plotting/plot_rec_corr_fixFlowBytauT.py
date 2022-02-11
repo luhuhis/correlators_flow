e28f658a5367ccb3851aa2b9b9c0363e5a76f22f
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-02-11 18:05:08 +0100

    plot_rec_corr_fixFlowBytauT.py: also plot just the UV contribution to the correlator. now expects a PhiUV file for each fitparam file.

--- plotting/plot_rec_corr_fixFlowBytauT.py
+++ plotting/plot_rec_corr_fixFlowBytauT.py
@@ -5,6 +5,7 @@ import numpy
 import scipy.integrate
 import scipy.interpolate
 import argparse
+import itertools
 
 
 def Gnorm(tauT):
@@ -17,6 +18,10 @@ def Gnorm(tauT):
 
 def model3(OmegaByT, kappa, PhiuvByT3, UV_prefactor):
     return numpy.maximum(PhiIR(OmegaByT, kappa), UV_prefactor * PhiuvByT3(OmegaByT))
+
+
+def UV_part(OmegaByT, PhiuvByT3, UV_prefactor):
+    return UV_prefactor * PhiuvByT3(OmegaByT)
 
 
 def PhiIR(OmegaByT, kappa):
@@ -32,7 +37,7 @@ def Integrand(OmegaByT, tauT, spf, *args):
 
 
 # find frankenstein continuum correlator at various fixed flowradiusBytauT
-def find_and_plot_and_save_frankenstein(flowradiusBytauT_list, possible_tauTs, y_int_list, e_int_list, out_file_path, ax, color_list):
+def find_and_plot_and_save_frankenstein(flowradiusBytauT_list, possible_tauTs, y_int_list, e_int_list, out_file_path, ax, color_list, Glabel):
     # find frankenstein continuum correlator at various fixed flowradiusBytauT
     frankenstein_arr = []
     for f, flowradiusBytauT in enumerate(flowradiusBytauT_list):
@@ -53,14 +58,14 @@ def find_and_plot_and_save_frankenstein(flowradiusBytauT_list, possible_tauTs, y
                       numpy.column_stack((frankenstein_xdata, frankenstein_ydata, frankenstein_edata)),
                       header="tauT      G/Gnorm_sqrt(8tauF)/tau="+r'{0:.3f}'.format(flowradiusBytauT)+"       err")
         ax.errorbar(frankenstein_xdata, frankenstein_ydata, frankenstein_edata, zorder=-len(flowradiusBytauT_list)+f,
-                    **lpd.chmap(lpd.plotstyle_add_point, markersize=0.2, fmt='x', color=color_list[f+1],
-                                label=r'$G_{\sqrt{8\tau_F}/\tau='+r'{0:.3f}'.format(flowradiusBytauT)+r'}}$'))  #
+                    **lpd.chmap(lpd.plotstyle_add_point, markersize=0, elinewidth=0.5, mew=0.5, fmt='x', color=color_list[f],
+                                label=r'$G_\mathrm{' + Glabel + r'}^{\sqrt{8\tau_\text{F}}/\tau='+r'{0:.3f}'.format(flowradiusBytauT)+r'}$'))  #
 
 
 def main():
     parser = argparse.ArgumentParser()
 
-    # TODO: generalize all the hardcoded paths
+    # TODO: generalize all the hardcoded paths, especially the output folder and output plot name!
 
     parser.add_argument('--fitparam_files', nargs='*', help='plot model 3 spf for these fit params', required=True)
     parser.add_argument('--use_tex', action="store_true")
@@ -68,64 +73,76 @@ def main():
     parser.add_argument('--ylims', help='custom ylims', nargs=2, type=float, default=(-0.03, 4))
     parser.add_argument('--xlims', help='custom xlims', nargs=2, type=float, default=(-0.01, 0.51))
     parser.add_argument('--flowradiusBytauT', nargs='*', type=float, help='which sqrt(8tauF)/tauT to plot', required=True)
-    parser.add_argument('--min_tauT', default=0.2, type=float, help='from which tauT on the reconstructed corr should be plotted')
-    parser.add_argument('--PhiUV_path', type=str, help='path to PhiUV file. expects three columns: omega/T, PhiUVByT3, err',
-                        default="/work/data/htshu/ee_spf/PHIUV_a.dat")
+    parser.add_argument('--PhiUV_files', type=str, nargs='*', help='path to PhiUV file. expects three columns: omega/T, PhiUVByT3, err', required=True)
     parser.add_argument('--plot_quenched_extr', help='whether to plot the 1.5 Tc quenched continuum/flow-extr EE data', action="store_true")  # TODO soften hard code
+    parser.add_argument('--title', help='title of plot', default=r'quenched, $1.5 T_c$, IR=$\kappa \omega /2T$', type=str)
 
     # when you want to load finite lattice spacing
     parser.add_argument('--qcdtype', help="list of qcdtypes. format doesnt matter, only used for finding data. example: quenched_1.50Tc_zeuthenFlow", nargs='*', type=str)
     parser.add_argument('--corr', help="list of corrs. choose from EE, EE_clover, BB, BB_clover", nargs='*', type=str)
     parser.add_argument('--conftype', nargs='*', type=str, help="list of conftypes, e.g. s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400")
+    parser.add_argument('--min_tauT', default=0.2, type=float, help='lower limit from which tauT on the reconstructed corr is valid/should be plotted. can be '
+                                                                    'overwritten by larger tauT parsed from fitparam file.')
 
     args = parser.parse_args()
 
-    # load PhiUV data
-    PhiUVa = numpy.loadtxt(args.PhiUV_path, unpack=True)[0:3]
-    PhiUVaByT3 = scipy.interpolate.InterpolatedUnivariateSpline(PhiUVa[0], PhiUVa[1], k=3)
-
-    MinOmegaByT = 0
-    MaxOmegaByT = PhiUVa[0][-1]
-
-    # calc reconstructed corrs for different parameters
-    xpoints = numpy.linspace(args.min_tauT, 0.5, args.npoints)
+    # calc reconstructed corrs for all given fitparam and PhiUV files.
+    xpoints_arr = []
     model3_means = []
     model3_uppers = []
     model3_lowers = []
     fitparams = []
-    for file in args.fitparam_files:
-        params, err_left, err_right = numpy.loadtxt(file, unpack=True)
+    just_UVs = []
+    for fitparam_file, PhiUV_file in zip(args.fitparam_files, args.PhiUV_files):
+        # load PhiUV
+        PhiUVa = numpy.loadtxt(PhiUV_file, unpack=True)[0:3]
+        PhiUVaByT3 = scipy.interpolate.InterpolatedUnivariateSpline(PhiUVa[0], PhiUVa[1], k=3)
+        MinOmegaByT = 0
+        MaxOmegaByT = PhiUVa[0][-1]
+
+        # find out lower tauT based on file path
+        tmp = lpd.remove_left_of_last('/', fitparam_file)
+        tmp = lpd.remove_right_of_last('.dat', tmp)
+        tmp = "0"+lpd.remove_left_of_first('_0', tmp)
+        this_mintauT = float(lpd.remove_right_of_first('_', tmp))
+        this_mintauT = numpy.fmax(args.min_tauT, this_mintauT)
+        xpoints_arr.append(numpy.linspace(this_mintauT, 0.5, args.npoints))
+        params, err_left, err_right = numpy.loadtxt(fitparam_file, unpack=True)
         fitparams.append(numpy.column_stack((params, err_left, err_right)))
         model3_mean = []
         model3_lower = []
         model3_upper = []
-        for flowindex in xpoints:
+        just_UV = []
+        for flowindex in xpoints_arr[-1]:
             model3_mean.append(scipy.integrate.quad(
                 lambda OmegaByT: Integrand(OmegaByT, flowindex, model3, params[0], PhiUVaByT3, params[1]), MinOmegaByT, MaxOmegaByT)[0] / Gnorm(flowindex))
             model3_lower.append(scipy.integrate.quad(
                 lambda OmegaByT: Integrand(OmegaByT, flowindex, model3, params[0]-err_left[0], PhiUVaByT3, params[1]-err_left[1]), MinOmegaByT, MaxOmegaByT)[0] / Gnorm(flowindex))
             model3_upper.append(scipy.integrate.quad(
                 lambda OmegaByT: Integrand(OmegaByT, flowindex, model3, params[0]+err_right[0], PhiUVaByT3, params[1]+err_right[1]), MinOmegaByT, MaxOmegaByT)[0] / Gnorm(flowindex))
+            just_UV.append(scipy.integrate.quad(
+                lambda OmegaByT: Integrand(OmegaByT, flowindex, UV_part, PhiUVaByT3, params[1]), MinOmegaByT,
+                MaxOmegaByT)[0] / Gnorm(flowindex))
         model3_means.append(model3_mean)
         model3_lowers.append(model3_lower)
         model3_uppers.append(model3_upper)
+        just_UVs.append(just_UV)
 
     # prepare plot canvas
     fig, ax, _ = lpd.create_figure(xlims=args.xlims, ylims=args.ylims, xlabel=r'$\tau T$', xlabelpos=(0.95, 0.05), ylabelpos=(0.08, 0.9),
                                    UseTex=args.use_tex,
-                                   ylabel=r'$\frac{G^*}{ G^\mathrm{norm} }$', figsize=(1.5 * (3 + 3 / 8), 1.5 * (3 + 3 / 8) / 16 * 9))
+                                   ylabel=r'$\frac{G}{ G^\mathrm{norm} }$', figsize=(1.5 * (3 + 3 / 8), 1.5 * (3 + 3 / 8) / 16 * 9))
 
-    # TODO generalize the color output somehow
-    colors = []
-    for i in range(len(model3_means)):
-        colors.append(lpd.get_color(range(len(model3_means)), i, 0, -1, 0.7))
+    colors = ('k', 'C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9')
 
+    color_offset = 0
     if args.plot_quenched_extr:
+        color_offset = 1
         # plot cont+flow extr. corr
         # TODO abstract this or make a flag to plot this or not?
         XX_flow_extr = numpy.loadtxt("/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/EE_flow_extr.txt", unpack=True)
-        ax.errorbar(XX_flow_extr[0], XX_flow_extr[1], XX_flow_extr[2], zorder=0, **lpd.chmap(lpd.plotstyle_add_point, color=colors[0], fmt='x', markersize=0.2,
-                                                                                             label=r'$G^{\tau_F\rightarrow 0}_\mathrm{cont}$'))
+        ax.errorbar(XX_flow_extr[0], XX_flow_extr[1], XX_flow_extr[2], zorder=0, **lpd.chmap(lpd.plotstyle_add_point, color='k', fmt='x', markersize=0, mew=0.5, elinewidth=0.5,
+                                                                                             label=r'$G^{\tau_\text{F}\rightarrow 0}_\mathrm{cont}$'))
 
         # load continuum quenched data and interpolate it
         EE_cont_arr = []
@@ -135,9 +152,9 @@ def main():
         stop = False
         for flowradius in flowradii:
             flowradius_str = r'{0:.4f}'.format(flowradius)
-            file = "/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_" + flowradius_str + "_cont.txt"
+            fitparam_file = "/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_" + flowradius_str + "_cont.txt"
             try:
-                EE_cont_arr.append(numpy.loadtxt(file, unpack=True))
+                EE_cont_arr.append(numpy.loadtxt(fitparam_file, unpack=True))
                 stop = True
                 max_idx += 1
             except OSError:
@@ -165,42 +182,51 @@ def main():
                 cont_int.append(None)
                 cont_err_int.append(None)
         find_and_plot_and_save_frankenstein(args.flowradiusBytauT, possible_tauTs, cont_int, cont_err_int,
-                                            "/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/", ax, colors)
+                                            "/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/", ax, colors, r'cont')
 
     # find frankenstein correlator at various fixed flowradiusBytauT
-    for qcdtype, corr, conftype in zip(args.qcdtype, args.corr, args.conftype):
-        inputfolder = lpd.get_merged_data_path(qcdtype, corr, conftype)
-        these_flowradii = numpy.loadtxt(inputfolder + "flowradii_" + conftype + ".dat")
-        XX = numpy.loadtxt(inputfolder + corr + "_" + conftype + ".dat")
-        XX_err = numpy.loadtxt(inputfolder + corr + "_err_" + conftype + ".dat")
-        beta, ns, nt, nt_half = lpd.parse_conftype(conftype)
-        these_tauT = numpy.arange(1/nt, 0.501, 1/nt)
+    if args.qcdtype is not None and args.corr is not None and args.conftype is not None:
+        for qcdtype, corr, conftype, color in itertools.zip_longest(args.qcdtype, args.corr, args.conftype, colors[color_offset:color_offset+len(args.conftype)]):
+            if qcdtype is None:
+                qcdtype = args.qcdtype[0]
+            if corr is None:
+                corr = args.corr[0]
+            inputfolder = lpd.get_merged_data_path(qcdtype, corr, conftype)
+            these_flowradii = numpy.loadtxt(inputfolder + "flowradii_" + conftype + ".dat")
+            XX = numpy.loadtxt(inputfolder + corr + "_" + conftype + ".dat")
+            XX_err = numpy.loadtxt(inputfolder + corr + "_err_" + conftype + ".dat")
+            beta, ns, nt, nt_half = lpd.parse_conftype(conftype)
+            these_tauT = numpy.arange(1/nt, 0.501, 1/nt)
 
-        # interpolate between flow times
-        y_int = []
-        e_int = []
-        for i in range(len(these_tauT)):
-            ydata = numpy.asarray(XX[:, i] * lpd.improve_corr_factor(i, nt))
-            edata = numpy.asarray(XX_err[:, i] * lpd.improve_corr_factor(i, nt))
-            xdata = these_flowradii
-            y_int.append(scipy.interpolate.InterpolatedUnivariateSpline(xdata, ydata, k=3, ext=2, check_finite=True))
-            e_int.append(scipy.interpolate.InterpolatedUnivariateSpline(xdata, edata, k=3, ext=2, check_finite=True))
+            # interpolate between flow times
+            y_int = []
+            e_int = []
+            for i in range(len(these_tauT)):
+                ydata = numpy.asarray(XX[:, i] * lpd.improve_corr_factor(i, nt))
+                edata = numpy.asarray(XX_err[:, i] * lpd.improve_corr_factor(i, nt))
+                xdata = these_flowradii
+                y_int.append(scipy.interpolate.InterpolatedUnivariateSpline(xdata, ydata, k=3, ext=2, check_finite=True))
+                e_int.append(scipy.interpolate.InterpolatedUnivariateSpline(xdata, edata, k=3, ext=2, check_finite=True))
 
-        find_and_plot_and_save_frankenstein(args.flowradiusBytauT, these_tauT, y_int, e_int,
-                                            inputfolder, ax, colors)
+            find_and_plot_and_save_frankenstein(args.flowradiusBytauT, these_tauT, y_int, e_int,
+                                                inputfolder, ax, (color,), r'N_\tau='+str(nt))
 
     # plot reconstructed correlators with "error bands"
-    for model3_mean, model3_upper, model3_lower, fitparam, color in zip(model3_means, model3_uppers, model3_lowers, fitparams, colors):
-        ax.fill_between(xpoints, model3_lower, model3_upper, linewidth=0,  color=color, alpha=0.3, zorder=-1000)
-        ax.errorbar(xpoints, model3_mean, color=color, alpha=0.3,
-                    label="$G^{\\rho =\\mathrm{max(IR,c\\,UV_{a})}}$ \n $\\kappa/T^3=" + "{0:.3f}".format(fitparam[0][0])
+    for xpoints, model3_mean, model3_upper, model3_lower, just_UV, fitparam, color, zorder in zip(xpoints_arr, model3_means, model3_uppers, model3_lowers, just_UVs, fitparams, colors, range(len(xpoints_arr))):
+        # ax.fill_between(xpoints, model3_lower, model3_upper, linewidth=0,  color=color, alpha=0.3, zorder=-zorder-1000)
+        ax.errorbar(xpoints, model3_mean, color=color, alpha=1,
+                    label="$ G_\\mathrm{fit}^{\\rho =\\mathrm{max(IR,c\\,UV_{a})}}$ \n "
+                          "$ \\kappa/T^3=" + "{0:.3f}".format(fitparam[0][0])
                           + "\\pm " + "{0:.3f}".format(numpy.fmax(fitparam[0][1], fitparam[0][2]))
-                          + "$\n $c=" + "{0:.3f}".format(fitparam[1][0]) + "\\pm " + "{0:.3f}".format(numpy.fmax(fitparam[1][1], fitparam[1][2])) + "$",
-                    **lpd.chmap(lpd.plotstyle_add_point, fmt='--', zorder=-100))
+                          + "$\n $ c=" + "{0:.3f}".format(fitparam[1][0]) + "\\pm " + "{0:.3f}".format(numpy.fmax(fitparam[1][1], fitparam[1][2])) + "$ \n"
+                          + "$\\chi^2/\\mathrm{dof}=" + "{0:.1f}".format(fitparam[2][0]) + " \\pm " + "{0:.1f}".format(numpy.fmax(fitparam[2][1], fitparam[2][2])) +"$",
+                    **lpd.chmap(lpd.plotstyle_add_point, fmt='--', zorder=-zorder-100,))
+        ax.errorbar(xpoints, just_UV, color=color, alpha=0.25,
+                    **lpd.chmap(lpd.plotstyle_add_point, lw=0.4, fmt='-.', zorder=-zorder-10000,))
 
-    lpd.legendstyle.update(dict(loc="lower right", bbox_to_anchor=(1.01, 0.07), labelspacing=1))
-    ax.legend(title=r'', **lpd.chmap(lpd.legendstyle, bbox_to_anchor=(1, 0.5), loc='center left', ncol=1, fontsize=6))
-    ax.set_title(r'quenched, $1.5 T_c$, IR=$\kappa \omega /2T$', x=0.5, y=1, fontsize=8)
+    lpd.legendstyle.update(dict(loc="lower right", bbox_to_anchor=(1.01, 0.07), labelspacing=2))
+    ax.legend(title=r'$G=$', **lpd.chmap(lpd.legendstyle, bbox_to_anchor=(1, 0.5), loc='center left', ncol=1, fontsize=4))
+    ax.set_title(args.title, x=0.5, y=1, fontsize=8)
 
     fig.savefig("/work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/EE/EE_frankenstein_comparison.pdf")
 
