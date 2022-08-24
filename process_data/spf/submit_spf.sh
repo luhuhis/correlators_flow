fdde0e198bd17b79e638fe70560bd806e0eea629
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-08-24 14:41:32 +0200

    submit_spf.sh: update wrapper script for different use cases

--- process_data/spf/submit_spf.sh
+++ process_data/spf/submit_spf.sh
@@ -24,9 +24,10 @@ source ~/.bashrc
 
 Nts=( 36 32 28 24 20 )
 temps=( 196 220 251 296 352 )
-flows=( 0.25 0.30 0.20 )  #0.20 0.15
+flows=( 0.25 0.20 )  #0.20 0.15
 #OmegaByT_UVs=( 7.143 6.364 5.578 4.728 3.977 )
 
+if [ 1 -eq 2 ] ; then
 #    if [ ${Nts[idx]} == 24 ] ; then
 for flow in "${flows[@]}" ; do
     for idx in "${!Nts[@]}"; do
@@ -41,5 +42,48 @@ for flow in "${flows[@]}" ; do
                 done
             done
     #    fi
+    done
+done
+fi
+
+# continuum fits
+#for flow in "0.20" ; do
+#    for model in "--model pnorm --p 2" ; do #"--model max" "--model sum" "--model plaw --OmegaByT_IR 1.0 --OmegaByT_UV 6.28" ; do
+#        for PhiUV in "--PhiUVtype LO  --min_scale 2piT --omega_prefactor 1" ; do # "--PhiUVtype NLO --min_scale opt --omega_prefactor opt" ; do  #
+#            ./spf_reconstruct.py --add_suffix quenched_cont_f$flow --nsamples 500 --nproc 34 \
+#                    --output_path /home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_rel_flow/rel_flow/ \
+#                    --input_corr /home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_rel_flow/rel_flow/EE_relflow_${flow}.dat \
+#                    --min_tauT 0.35 $model \
+#                    --max_type smooth --T_in_GeV 0.472 --Nf 0 $PhiUV
+#        done
+#    done
+#done
+#fi
+
+# finite a and tf fits
+if [ 1 -eq 2 ] ; then
+conftypes=(s080t20_b0703500 s144t36_b0754400)  # 24 30
+Nts=(20 36)  # 24 30
+flow="0.20"
+for idx in "${!conftypes[@]}" ; do
+    for model in "--model pnorm --p 2" "--model max" "--model sum" "--model plaw --OmegaByT_IR 1.0 --OmegaByT_UV 6.28" ; do
+        for PhiUV in "--PhiUVtype LO  --min_scale 2piT --omega_prefactor 1" "--PhiUVtype NLO --min_scale opt --omega_prefactor opt" ; do  #
+            ./spf_reconstruct.py --add_suffix quenched_${conftypes[idx]}_f${flow} --nsamples 500 --nproc 34 \
+                        --output_path /home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/${conftypes[idx]}/rel_flow/ \
+                        --input_corr /home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/${conftypes[idx]}/rel_flow/EE_relflow_${flow}.dat \
+                        --min_tauT 0.35 $model \
+                        --max_type smooth --T_in_GeV 0.472 --Nf 0 $PhiUV
+        done
+    done
+done
+fi
+
+for model in "--model pnorm --p 2" "--model max" "--model sum" "--model plaw --OmegaByT_IR 1.0 --OmegaByT_UV 6.28" ; do
+    for PhiUV in "--PhiUVtype LO  --min_scale 2piT --omega_prefactor 1" "--PhiUVtype NLO --min_scale opt --omega_prefactor opt" ; do  #
+        ./spf_reconstruct.py --add_suffix quenched_BB --nsamples 500 --nproc 34 \
+            --output_path /home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/BB/ \
+            --input_corr /home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/BB/BB_flow_extr.txt \
+            --min_tauT 0 $model \
+            --max_type smooth --T_in_GeV 0.472 --Nf 0 $PhiUV
     done
 done
