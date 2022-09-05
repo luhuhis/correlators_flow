9bf1f5300ba59629dcc3f080efdf965eb10140e0
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-09-05 18:59:02 +0200

    update wrapper scripts

--- process_data/3_spline_interpolate.sh
+++ process_data/3_spline_interpolate.sh
@@ -1,65 +1,46 @@
 #!/bin/bash
 
-max_flow_idx=170
-min_flow_idx=0
-qcdtype=quenched_1.50Tc_zeuthenFlow
-conftypes="s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400" #""
-corrs="EE" # BB BB_clover EE
-int_Nt=36
-nsamples=1000
-max_FlowradiusBytauT=""  # "--max_FlowradiusBytauT 0.33"
+# "s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400" #""
+arr_conftypes=("s064t20_b0785700" "s064t20_b0803600" "s064t24_b0777700" "s064t24_b0791300")
+arr_int_Nt=(28 24 36 32)
+
+for idx in "${!arr_conftypes[@]}" ; do
+
+    conftypes=${arr_conftypes[idx]}
+    int_Nt=${arr_int_Nt[idx]}
+    max_flow_idx=100 #170
+    min_flow_idx=0
+    qcdtype=hisq_ms5_zeuthenFlow  # quenched_1.50Tc_zeuthenFlow
+    corrs="EE" # BB BB_clover EE
+    nsamples=1000
+    max_FlowradiusBytauT=""  # "--max_FlowradiusBytauT 0.33"
 
 
-for conftype in $conftypes; do
+    for conftype in $conftypes; do
+        for corr in $corrs; do
+            rm -f /home/altenkort/work/correlators_flow/plots/$qcdtype/$corr/$conftype/interpolations/*interpolation.pdf
+            rm -f /home/altenkort/work/correlators_flow//data/merged/$qcdtype/$corr/$conftype/interpolations/*interpolation.txt
+        done
+    done
+
+    args="--ylims 0 12 $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --int_Nt $int_Nt --nsamples $nsamples"
+
     for corr in $corrs; do
-        rm -f /home/altenkort/work/correlators_flow/plots/$qcdtype/$corr/$conftype/interpolations/*interpolation.pdf
-        rm -f /home/altenkort/work/correlators_flow//data/merged/$qcdtype/$corr/$conftype/interpolations/*interpolation.txt
-    done
-done
-
-
-for corr in $corrs; do
-    for conftype in $conftypes ; do
-        for ((i=min_flow_idx; i < max_flow_idx; i+=10)) ; do
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i))   --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+1)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+2)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+3)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+4)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+5)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+6)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+7)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+8)) --int_Nt $int_Nt --nsamples $nsamples &
-            /usr/local/bin/python3.7m -u _3_spline_interpolate.py  $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --flow_index $((i+9)) --int_Nt $int_Nt --nsamples $nsamples &
-            wait
+        for conftype in $conftypes ; do
+            for ((i=min_flow_idx; i < max_flow_idx; i+=10)) ; do
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i))   &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+1)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+2)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+3)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+4)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+5)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+6)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+7)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+8)) &
+                python3 -u _3_spline_interpolate.py $args --flow_index $((i+9)) &
+                wait
+            done
         done
     done
+
 done
-
-sbatchscript=$(cat <<EOF
-#!/bin/bash
-#SBATCH --job-name=spline_interpolate
-#SBATCH --output=./%x.out
-#SBATCH --error=./%x.err
-#SBATCH --time=00:30:00
-#SBATCH --partition=volta
-#SBATCH --qos=compute_cpu
-#SBATCH --ntasks=1
-#SBATCH --array=0-600
-#SBATCH --open-mode=append
-
-export PATH=\$PATH:/usr/local/texlive/2020/bin/x86_64-linux
-parameters=()
-for corr in $corrs; do
-    for conftype in $conftypes ; do
-        for i in {0..$max_flow_idx} ; do
-            parameters+=("--qcdtype $qcdtype --conftype \$conftype --corr \$corr --flow_index \$i")
-        done
-    done
-done
-echo "\${parameters[\$SLURM_ARRAY_TASK_ID]}"
-srun python -u _3_spline_interpolate.py  \${parameters[\$SLURM_ARRAY_TASK_ID]}
-
-EOF
-)
-#sbatch <(cat <<< "$sbatchscript")
--- process_data/4_continuum_extr.sh
+++ process_data/4_continuum_extr.sh
@@ -1,56 +1,48 @@
 #!/bin/bash
 
+#conftypes="s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400"
+
+arr_conftypes=(\
+    "s064t20_b0785700 s096t28_b0824900_m002022_m01011"\
+    "s064t20_b0803600 s096t24_b0824900_m002022_m01011"
+    "s064t24_b0777700 s096t36_b0824900_m002022_m01011"
+    "s064t24_b0791300 s096t32_b0824900_m002022_m01011")
+
+arr_output_suffix=("T251" "T296" "T196" "T220")
+
 #parameters
-max_flow_idx=170 #135
-min_flow_idx=0 #50
-qcdtype=quenched_1.50Tc_zeuthenFlow
-conftypes="s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400"
+max_flow_idx=100 # 170 #135
+min_flow_idx=20 #50
+qcdtype=hisq_ms5_zeuthenFlow  # quenched_1.50Tc_zeuthenFlow
+
+ylims="0 12" # $"1.5 4"
 corrs="EE" # BB_clover EE BB
 int="" #--int_only --int_Nt 64"
 tex="" #"--use_tex"
 max_FlowradiusBytauT=""  # "--max_FlowradiusBytauT 0.5"
 
-for corr in $corrs; do
-    rm -f /home/altenkort/work/correlators_flow/plots/$qcdtype/$corr/cont_extr*/*.pdf
-    rm -f /home/altenkort/work/correlators_flow//data/merged/$qcdtype/$corr/cont_extr*/*.txt
+for idx in "${!arr_conftypes[@]}" ; do
+
+    for corr in $corrs; do
+        rm -f /home/altenkort/work/correlators_flow/plots/$qcdtype/$corr/${arr_output_suffix[idx]}/cont_extr*/*.pdf
+        rm -f /home/altenkort/work/correlators_flow//data/merged/$qcdtype/$corr/${arr_output_suffix[idx]}/cont_extr*/*.txt
+    done
+
+    args="--output_suffix ${arr_output_suffix[idx]} --qcdtype $qcdtype --conftypes ${arr_conftypes[idx]@Q} --corr $corr $tex --custom_ylims $ylims $max_FlowradiusBytauT $int"
+
+    for ((i=min_flow_idx; i < max_flow_idx; i+=10)) ; do
+      for corr in $corrs; do
+        python3 -u _4_continuum_extr.py $args --flow_index $((i))   &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+1)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+2)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+3)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+4)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+5)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+6)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+7)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+8)) &
+        python3 -u _4_continuum_extr.py $args --flow_index $((i+9)) &
+        wait
+      done
+    done
 done
-
-for ((i=min_flow_idx; i < max_flow_idx; i+=10)) ; do
-  for corr in $corrs; do
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i)) $tex --custom_ylims 1.5 4   $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+1)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+2)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+3)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+4)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+5)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+6)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+7)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+8)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    /usr/local/bin/python3.7m -u _4_continuum_extr.py  --qcdtype $qcdtype --conftypes $conftypes --corr $corr --flow_index $((i+9)) $tex --custom_ylims 1.5 4 $max_FlowradiusBytauT $int &
-    wait
-  done
-done
-#wait
-
-
-#deprecated:
-sbatchscript=$(cat <<EOF
-#! /bin/bash
-#SBATCH --job-name=cont_extr
-#SBATCH --output=./%x.out
-#SBATCH --error=./%x.err
-#SBATCH --time=01:00:00
-#SBATCH --partition=devel_cpu
-#SBATCH --ntasks=1
-#SBATCH --array=0-83
-parameters=()
-for i in {50..133} ; do
-    parameters+=("\$i")
-done
-
-export PATH=\$PATH:/usr/local/texlive/2020/bin/x86_64-linux
-srun python -u _4_continuum_extr.py \${parameters[\$SLURM_ARRAY_TASK_ID]}
-
-EOF
-)
-#sbatch <(cat <<< "$sbatchscript")
