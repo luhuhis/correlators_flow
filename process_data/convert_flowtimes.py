4c6189e1643198480a6e707d4ee752f1bed3284c
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-10-05 17:14:44 +0200

    convert_flowtimes.py: add option to convert between different lattice spacings

--- process_data/convert_flowtimes.py
+++ process_data/convert_flowtimes.py
@@ -9,16 +9,33 @@ def main():
     parser = argparse.ArgumentParser("script to convert flow times at fixed temperature from one lattice spacing to another")
     parser.add_argument('--input', type=str, help="path to input file that contains dimensionless lattice flow times t_F = tau_F / a^2", required=True)
     parser.add_argument('--output', type=str, help="path to output file", required=True)
-    parser.add_argument('--input_Nt', type=float, help="Nt of input lattice at temperature T", required=True)
-    parser.add_argument('--output_Nt', type=float, help="Nt of output lattice at temperature T", required=True)
+    parser.add_argument('--input_Nt', type=float, help="Nt of input lattice at fixed temperature T")
+    parser.add_argument('--output_Nt', type=float, help="Nt of output lattice at fixed temperature T", required=True)
+    parser.add_argument('--input_a', type=float, help="lattice spacing of input lattice")
+    parser.add_argument('--output_a', type=float, help="lattice spacing of output lattice")
     parser.add_argument('--threshold', default=0.3, type=float, help="maximum flow RADIUS in units of temperature")
+    parser.add_argument('--type', choices=["fixed_temperature", "different_temperature"])
     args = parser.parse_args()
+
+    if args.type == "fixed_temperature":
+        if not args.input_Nt or not args.output_Nt:
+            print("ERROR: For type=fixed_temperature need input_Nt and output_Nt")
+            exit(1)
+    if args.type == "different_temperature":
+        if not args.input_a or not args.output_a:
+            print("ERROR: For type=different_temperature need input_a and output_a")
+            exit(1)
+        if args.input_a > args.output_a:
+            print("WARN: input_a should be smaller than output_a, to ensure that stepsizes do not increase.")
 
     flowtimes_input = numpy.loadtxt(args.input)
     flowtimes_output = []
     one_more = True
     for tf in flowtimes_input:
-        tf_prime = tf/args.input_Nt**2 * args.output_Nt**2
+        if args.type == "fixed_temperature":
+            tf_prime = tf/args.input_Nt**2 * args.output_Nt**2
+        elif args.type == "different_temperature":
+            tf_prime = tf * args.input_a ** 2 / args.output_a ** 2
         if numpy.sqrt(8*tf_prime)/args.output_Nt <= args.threshold:
             flowtimes_output.append(tf_prime)
         elif one_more:
