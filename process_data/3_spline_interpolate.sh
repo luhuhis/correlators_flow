c12ba73b81d23d79d5c1a9fda057b8ea0dc8bf8e
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-10-27 12:10:43 -0700

    finally make wrapper scripts more general

--- process_data/3_spline_interpolate.sh
+++ process_data/3_spline_interpolate.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
-# "s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400" #""
-arr_conftypes=("s064t20_b0785700" "s064t20_b0803600" "s064t24_b0777700" "s064t24_b0791300")
-arr_int_Nt=(28 24 36 32)
+qcdtype=$1
 
-for idx in "${!arr_conftypes[@]}" ; do
+if [ -z "$qcdtype" ] ; then
+    echo "Usage: $0 qcdtype"
+    echo "choices for qcdtype: quenched_1.50Tc_zeuthenFlow hisq_ms5_zeuthenFlow"
+    exit
+fi
 
-    conftypes=${arr_conftypes[idx]}
-    int_Nt=${arr_int_Nt[idx]}
-    max_flow_idx=100 #170
-    min_flow_idx=0
-    qcdtype=hisq_ms5_zeuthenFlow  # quenched_1.50Tc_zeuthenFlow
-    corrs="EE" # BB BB_clover EE
-    nsamples=1000
-    max_FlowradiusBytauT=""  # "--max_FlowradiusBytauT 0.33"
+ncpu=20
+nsamples=1000
+min_flow_idx=0
 
+if [ "$qcdtype" == quenched_1.50Tc_zeuthenFlow ] ; then
+    arr_conftypes=("s080t20_b0703500" "s096t24_b0719200" "s120t30_b0739400")
+    arr_int_Nt=(36 36 36)
+    max_flow_idx=170 #100
+    corrs="EE"
+    ylims="0 5"
+    add_args="--max_FlowradiusBytauT 0.5 --max_FlowradiusBytauT_offset=0"
+elif [ "$qcdtype" == hisq_ms5_zeuthenFlow ] ; then
+    arr_conftypes=( "s064t20_b0785700" "s064t24_b0806800" "s064t20_b0803600" "s064t22_b0814700" "s064t20_b0770400" "s064t24_b0791300" "s064t20_b0757000" "s064t24_b0777700")
+    arr_int_Nt=(28 28 24 24 32 32 36 36)
+    max_flow_idx=100
+    corrs="EE"
+    ylims="0 12"
+    add_args="--max_FlowradiusBytauT 0.5 --max_FlowradiusBytauT_offset=0"
+fi
 
-    for conftype in $conftypes; do
-        for corr in $corrs; do
-            rm -f /home/altenkort/work/correlators_flow/plots/$qcdtype/$corr/$conftype/interpolations/*interpolation.pdf
-            rm -f /home/altenkort/work/correlators_flow//data/merged/$qcdtype/$corr/$conftype/interpolations/*interpolation.txt
-        done
-    done
+for idx in "${!arr_conftypes[@]}"; do
+    conftype="${arr_conftypes[idx]}"
+    for corr in "${corrs[@]}"; do
+        rm -f /home/altenkort/work/correlators_flow/plots/$qcdtype/$corr/$conftype/interpolations/*interpolation.pdf
+        rm -f /home/altenkort/work/correlators_flow//data/merged/$qcdtype/$corr/$conftype/interpolations/*interpolation.txt
 
-    args="--ylims 0 12 $max_FlowradiusBytauT --qcdtype $qcdtype --conftype $conftype --corr $corr --int_Nt $int_Nt --nsamples $nsamples"
+        args="$add_args --ylims $ylims --qcdtype $qcdtype --conftype $conftype --corr $corr --int_Nt ${arr_int_Nt[idx]} --nsamples $nsamples"
 
-    for corr in $corrs; do
-        for conftype in $conftypes ; do
-            for ((i=min_flow_idx; i < max_flow_idx; i+=10)) ; do
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i))   &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+1)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+2)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+3)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+4)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+5)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+6)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+7)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+8)) &
-                python3 -u _3_spline_interpolate.py $args --flow_index $((i+9)) &
-                wait
+        for ((i=min_flow_idx; i < max_flow_idx; i+=ncpu)) ; do
+            for ((j=0; j < ncpu; j+=1)) ; do
+                python3 -u _3_spline_interpolate.py "$args" --flow_index $((i+j)) &
             done
+            wait
         done
     done
-
 done
+
--- process_data/4_continuum_extr.sh
+++ process_data/4_continuum_extr.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-#conftypes="s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400"
+qcdtype=$1
 
-arr_conftypes=(\
-    "s064t20_b0785700 s096t28_b0824900_m002022_m01011"\
-    "s064t20_b0803600 s096t24_b0824900_m002022_m01011"
-    "s064t24_b0777700 s096t36_b0824900_m002022_m01011"
-    "s064t24_b0791300 s096t32_b0824900_m002022_m01011")
+if [ -z "$qcdtype" ] ; then
+    echo "Usage: $0 qcdtype"
+    echo "choices for qcdtype: quenched_1.50Tc_zeuthenFlow hisq_ms5_zeuthenFlow"
+    exit
+fi
 
-arr_output_suffix=("T251" "T296" "T196" "T220")
+ncpu=20
 
-#parameters
-max_flow_idx=100 # 170 #135
-min_flow_idx=20 #50
-qcdtype=hisq_ms5_zeuthenFlow  # quenched_1.50Tc_zeuthenFlow
-
-ylims="0 12" # $"1.5 4"
-corrs="EE" # BB_clover EE BB
-int="" #--int_only --int_Nt 64"
-tex="" #"--use_tex"
-max_FlowradiusBytauT=""  # "--max_FlowradiusBytauT 0.5"
+if [ "$qcdtype" == quenched_1.50Tc_zeuthenFlow ] ; then
+    arr_conftypes=("s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400")
+    arr_output_suffix=("" )
+    ylims="1.5 4"
+    max_flow_idx=170
+    min_flow_idx=0
+    corrs="EE" # BB_clover EE BB
+    add_args=""
+elif [ "$qcdtype" == hisq_ms5_zeuthenFlow ] ; then
+    arr_conftypes=(\
+    "s064t20_b0770400 s064t24_b0791300 s096t32_b0824900_m002022_m01011"
+    "s064t20_b0785700 s064t24_b0806800 s096t28_b0824900_m002022_m01011"
+    "s064t20_b0803600 s064t22_b0814700 s096t24_b0824900_m002022_m01011"
+    "s064t20_b0757000 s064t24_b0777700 s096t36_b0824900_m002022_m01011")
+    arr_output_suffix=("T220" "T251" "T296" "T196")
+    ylims="0 12"
+    max_flow_idx=170
+    min_flow_idx=0
+    corrs="EE" # BB_clover EE BB
+fi
 
 for idx in "${!arr_conftypes[@]}" ; do
 
@@ -28,21 +38,18 @@ for idx in "${!arr_conftypes[@]}" ; do
         rm -f /home/altenkort/work/correlators_flow//data/merged/$qcdtype/$corr/${arr_output_suffix[idx]}/cont_extr*/*.txt
     done
 
-    args="--output_suffix ${arr_output_suffix[idx]} --qcdtype $qcdtype --conftypes ${arr_conftypes[idx]@Q} --corr $corr $tex --custom_ylims $ylims $max_FlowradiusBytauT $int"
+    if [ "${arr_output_suffix[idx]}" ] ; then
+        sufargs="--output_suffix"
+    fi
 
-    for ((i=min_flow_idx; i < max_flow_idx; i+=10)) ; do
-      for corr in $corrs; do
-        python3 -u _4_continuum_extr.py $args --flow_index $((i))   &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+1)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+2)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+3)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+4)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+5)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+6)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+7)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+8)) &
-        python3 -u _4_continuum_extr.py $args --flow_index $((i+9)) &
-        wait
-      done
+    args="$add_args --max_FlowradiusBytauT_offset 0 --max_FlowradiusBytauT 0.5  $sufargs ${arr_output_suffix[idx]} --qcdtype $qcdtype --conftypes ${arr_conftypes[idx]} --corr $corr --custom_ylims $ylims"
+
+    for ((i=min_flow_idx; i < max_flow_idx; i+=20)) ; do
+        for corr in $corrs; do
+            for ((j=0; j < ncpu; j+=1)) ; do
+                python3 -u _4_continuum_extr.py $args --flow_index $((i+j))   &
+            done
+            wait
+        done
     done
 done
