cf61f92f327b1730b358a96fa0cdd9b905e3da26
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-11-15 13:11:34 +0100

    _2_reduce_data.py: save samples and correlation matrix in npy format

--- process_data/_2_reduce_data.py
+++ process_data/_2_reduce_data.py
@@ -1,10 +1,10 @@
-#!/usr/local/bin/python3.7m -u
+#!/usr/bin/env python3
 import numpy
 import lib_process_data as lpd
 from latqcdtools.statistics import bootstr
 
 
-def load_merged_data(qcdtype, corr, conftype):
+def load_merged_data(qcdtype, corr, conftype, basepath):
     """
     load and reorganize data
 
@@ -13,7 +13,7 @@ def load_merged_data(qcdtype, corr, conftype):
     XX_data: first index [0]=XX_numerator_real, [1]=polyakov_real, second index = different measurements (-> pairs of the form (XX_numerator_real[x], XX_polyakov_real[x]))
     the bootstrap leaves out pairs of (XX_numerator, Polyakov), this is done by the last argument (1), which specifies the axis on which the data pairs lie.
     """
-    inputfolder = lpd.get_merged_data_path(qcdtype, corr, conftype)
+    inputfolder = lpd.get_merged_data_path(qcdtype, corr, conftype, basepath)
     nt, nt_half = lpd.parse_conftype(conftype)[2:]
 
     print("read  "+inputfolder+"flowtimes_"+conftype+".dat")
@@ -49,14 +49,10 @@ def compute_XX_corr(data):
     """
     function for bootstrap routine that computes an XX correlator (with XX=EE or BB) normalized by the polyakov loop. numerator data (i.e. --X--X--) is first index, polyakov loop is second index of data.
     """
-    normalization_factor = 1  # the multiplicities are now correctly taken care of inside of the ParallelGPUCode
     numerator_mean = numpy.mean(data[0], axis=0)
     denominator_mean = numpy.mean(data[1], axis=0)
-    XX = numerator_mean/normalization_factor / denominator_mean
-    numerator_std_dev = numpy.std(data[0], axis=0)
-    denominator_std_dev = numpy.std(data[1], axis=0)
-    XX_std_dev = numpy.sqrt((numerator_std_dev/(normalization_factor*denominator_mean))**2+(numerator_mean*denominator_std_dev/(normalization_factor*denominator_mean**2))**2)
-    return XX, XX_std_dev
+    XX = numerator_mean/denominator_mean
+    return XX
 
 
 def compute_only_numerator(data):
@@ -75,16 +71,19 @@ def main():
     parser.add_argument('--part_obs', help="only compute part of the observable", choices=['numerator', 'polyakovloop'])
     parser.add_argument('--n_samples', help='number of bootstrap samples', type=int, default='1000')
     parser.add_argument('--BB_renorm', action="store_true", help="multiply corr with Z factor")
+    parser.add_argument('--basepath', default="", type=str, help="where to look for the data")
+    parser.add_argument('--nproc', default=10, type=int, help="how many processes to use for parallelization")
     args = parser.parse_args()
 
     beta, ns, nt, nt_half = lpd.parse_conftype(args.conftype)
 
-    inputfolder = lpd.get_merged_data_path(args.qcdtype, args.corr, args.conftype)
+    inputfolder = lpd.get_merged_data_path(args.qcdtype, args.corr, args.conftype, args.basepath)
     outputfolder = inputfolder
 
-    lpd.create_folder(outputfolder+"/btstrp_samples/")
-    flow_times, n_flow, n_datafiles, n_streams, XX_data = load_merged_data(args.qcdtype, args.corr, args.conftype)
+    flow_times, n_flow, n_datafiles, n_streams, XX_data = load_merged_data(args.qcdtype, args.corr, args.conftype, args.basepath)
+    # TODO: add ndiscard here, and not in merge data
     XX_data = numpy.asarray(XX_data)
+    print(XX_data.shape)
     tauTs = numpy.arange(1/nt, 0.501, 1/nt)
     n_samples = args.n_samples
 
@@ -97,32 +96,41 @@ def main():
         if args.part_obs == "polyakovloop":
             reduce_function = compute_only_denominator
             file_prefix += "_polyakovloop"
-    # Use same_rand_for_obs=False in order to break up correlations between the observables
-    XX_samples, XX, XX_err = bootstr.bootstr(reduce_function, XX_data, numb_samples=n_samples, sample_size=n_datafiles, conf_axis=1, return_sample=True, same_rand_for_obs=False, parallelize=True, nproc=40, seed=0)
 
-    # extract the right data
-    XX_samples_mean = numpy.asarray([i[0] for i in XX_samples])
-    XX_samples_err = numpy.asarray([i[1] for i in XX_samples])
-    XX_mean = XX[0]  # this is the bootstrap estimate for the mean of the correlator
-    XX_err = XX_err[0]  # this is the bootstrap estimate for the error of the mean of the correlator
+    # TODO add BB renorm Z factor in this step, not afterwards!
+    # Z = 1
+    # if args.BB_renorm and args.corr == "BB":
+    #     Z = Z2[i]
+
+    # Use same_rand_for_obs=False in order to break up correlations between the observables
+    XX_samples, XX, XX_err = bootstr.bootstr(reduce_function, XX_data, numb_samples=n_samples, sample_size=n_datafiles, conf_axis=1, return_sample=True, same_rand_for_obs=True, parallelize=True, nproc=args.nproc, seed=0, err_by_dist=True)
+    XX_samples = numpy.asarray(XX_samples)
+    print(XX_samples.shape)
+    # XX:     this is the bootstrap estimate for the mean of the correlator
+    # XX_err: this is the bootstrap estimate for the error of the mean of the correlator
+
+    # save samples
+    filename = outputfolder + file_prefix + "_" + args.conftype + "_samples.npy"
+    print("write " + filename)
+    numpy.save(filename, XX_samples)
 
     # add renormalization factor for BB correlator. errors for it are extremely small. FIXME abstract this
     if args.BB_renorm and args.corr == "BB":
         path = "/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/coupling/"
         tfT2, Z2 = numpy.loadtxt(path + "Z2_" + str(nt) + ".dat", unpack=True)
-        for i in range(XX_mean.shape[0]):
-            for j in range(XX_mean.shape[1]):
-                XX_mean[i, j] *= Z2[i]
+        for i in range(XX.shape[0]):
+            for j in range(XX.shape[1]):
+                XX[i, j] *= Z2[i]
                 XX_err[i, j] *= Z2[i]
 
-    # write XX_mean and XX_err to file
+    # write XX and XX_err to file
     filename = outputfolder+file_prefix+"_"+args.conftype+".dat"
     print("write "+filename)
     with open(filename, 'w') as outfile:
         outfile.write('# bootstrap mean of '+str(n_datafiles)+' measurements of '+args.corr+' correlator for '+args.qcdtype+' '+args.conftype+'\n')
         outfile.write('# rows correspond to flow times, columns to dt = {1, ... , Ntau/2}\n')
         lpd.write_flow_times(outfile, flow_times)
-        numpy.savetxt(outfile, XX_mean)
+        numpy.savetxt(outfile, XX)
 
     filename = outputfolder+file_prefix+"_err_"+args.conftype+".dat"
     print("write "+filename)
@@ -132,28 +140,16 @@ def main():
         lpd.write_flow_times(outfile, flow_times)
         numpy.savetxt(outfile, XX_err)
 
-    # write bootstrap samples in files for each flowtime separately
-    for i in range(n_flow):
-        filename = outputfolder+"/btstrp_samples/"+file_prefix+"_"+'{0:.4f}'.format(numpy.sqrt(flow_times[i]*8)/nt)+"_Nt"+str(nt)+"_btstrp_samples.dat"
-        print("write "+filename)
-
-        # add BB renorm
-        Z = 1
-        if args.BB_renorm and args.corr == "BB":
-            Z = Z2[i]
-
-        with open(filename, 'w') as outfile:
-            outfile.write("# "+str(n_samples)+" bootstrap samples for "+args.corr+" correlator "+args.qcdtype+" "+args.conftype+"\n# one sample consists of "+str(n_datafiles)+" draws(measurements), which are then reduced\n")
-            outfile.write("# flowtime="+str(flow_times[i])+" or flowradius="+str(numpy.sqrt(flow_times[i]*8)/nt)+'\n')
-            outfile.write("# first column: \\tau_T, second column: G(tauT), third column: std_dev\n")
-            for k in range(n_samples):
-                XX_sample_single_flowtime = numpy.empty((nt_half, 3))
-                for j in range(nt_half):
-                    XX_sample_single_flowtime[j, 0] = tauTs[j]
-                    XX_sample_single_flowtime[j, 1] = XX_samples_mean[k][i, j] * Z
-                    XX_sample_single_flowtime[j, 2] = XX_samples_err[k][i, j] * Z
-                numpy.savetxt(outfile, XX_sample_single_flowtime)
-                outfile.write('#\n')
+    # for each tau, compute flow correlation matrix.
+    XX_samples = numpy.swapaxes(XX_samples, 0, 2)  # change data structure such that numpy.cov understands it
+    pcov = []
+    for i in range(nt_half):
+        pcov.append(numpy.corrcoef(XX_samples[i]))
+    pcov = numpy.asarray(pcov)
+    filename = outputfolder + file_prefix + "_flow_cov_" + args.conftype + ".npy"
+    print("write " + filename)
+    numpy.save(filename, pcov)
+    print(pcov.shape)
 
     print("done reducing data for "+args.conftype+" "+args.corr)
 
