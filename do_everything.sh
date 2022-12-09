c63be2710dbb0330d9a908869213158ce94df3f6
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-12-09 18:31:25 +0100

    do_everything.sh's name is now a bit more truthful

--- do_everything.sh
+++ do_everything.sh
@@ -7,13 +7,23 @@
 ./process_data/plot_pert_latt.py --Nt 20 --corr EE --flowtime_file ../data/merged/pert_LO/flowtimes.dat --outputpath ../plots/pertLO/ --inputpath ../data/merged/pert_LO/ --tau 5 --exp
 
 
-qcdtypes=("quenched_1.5Tc_zeuthenFlow" "hisq_ms5_zeuthenFlow")
-for qcdtype in "${qcdtypes[@]}"; do
-# TODO add BB
-./process_data/example_usage/1_merge_data.sh $qcdtype EE
-./process_data/example_usage/1_xestimate_autocorrelations.sh $qcdtype EE
-./process_data/example_usage/3_spline_interpolate.sh $qcdtype
-./process_data/example_usage/4_continuum_extr.sh $qcdtype
-./process_data/example_usage/5_flowtime_extr.sh $qcdtype
+./plotting/plot_integrand.py --outputpath ../plots/ --PathPhiUV ../data/merged/quenched_1.50Tc_zeuthenFlow/EE/spf/max_LO_T0.472_min2piT_w1_500_0.0_exp0_quenched_cont_f0/phiUV.dat
 
+./plotting/plot_EEvsBB.py
+./process_data/covariance.py --qcdtype quenched_1.50Tc_zeuthenFlow --conftype s144t36_b0754400 --corr EE --basepath ../data/merged/ --outputfolder ../plots/quenched_1.50Tc_zeuthenFlow/EE/
+./plotting/plot_kappaB.py --outputfolder ../plots/quenched_1.50Tc_zeuthenFlow/BB/
+
+
+qcdtypes=("quenched_1.5Tc_zeuthenFlow" "hisq_ms5_zeuthenFlow")
+corrs=("EE" "BB")
+for corr in "${corrs[@]}" ; do
+    for qcdtype in "${qcdtypes[@]}"; do
+        ./process_data/example_usage/1_merge_data.sh $qcdtype $corr
+        ./process_data/example_usage/1_xestimate_autocorrelations.sh $qcdtype EE
+        ./process_data/example_usage/3_spline_interpolate.sh $qcdtype
+        ./process_data/example_usage/4_continuum_extr.sh $qcdtype
+        ./process_data/example_usage/5_flowtime_extr.sh $qcdtype
+    done
 done
+
+./plotting/6_plot_finalcorr.py --outputfolder ../plots/quenched_1.50Tc_zeuthenFlow/EE/ --input_flow ../data/merged/quenched_1.50Tc_zeuthenFlow/EE/EE_flow_extr.txt --input_multilvl ../data/merged/quenched_1.50Tc_zeuthenFlow/EE/multi-level_2015/EE_2015_new.txt
