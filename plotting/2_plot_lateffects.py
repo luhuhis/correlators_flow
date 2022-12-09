0b026e5172ce40643d017783829b58381e6b0bcf
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-12-09 18:32:01 +0100

    completely refactor and modernize 2_plot_lateffects.py

--- plotting/2_plot_lateffects.py
+++ plotting/2_plot_lateffects.py
@@ -1,114 +1,138 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 
-    #input
-    #   lpd.inputfolder+conftype+"/interpolations/XX_"+'{0:.3f}'.format(lpd.flow_radius[i])+"_interpolation.txt"
-    #   lpd.inputfolder+"/cont_extr/XX_"+'{0:.3f}'.format(lpd.flow_radius[i])+"_cont.txt"
-
-    #output
-    #   lpd.outputfolder+"/lattice_effects/XX_flow_"+'{0:.3f}'.format(lpd.flow_radius[i])+".pdf"
-
-import sys
+import argparse
 import numpy
 import matplotlib
-import itertools
 import lib_process_data as lpd
 from matplotlib import cm
+from matplotlib.backends.backend_pdf import PdfPages
+
+import warnings
+# # warnings.simplefilter("ignore", OptimizeWarning)
+# warnings.simplefilter('error', UserWarning)
+warnings.filterwarnings('ignore', r'(.*?)converting a masked element to nan.')
+warnings.filterwarnings('ignore', r'(.*?)More than 20 figures have been opened.(.*?)')
+
+
+def plot_single_flowtime(index, flowradii, args, tauT, XX, XX_err, labels, ylims):
+
+    nplot = len(XX)
+    colors = ['k', *[cm.tab10(i) for i in range(nplot)]]
+    # markers = ['s', 'o', 'D', 'H', 'h']
+    zorders = numpy.linspace(-50, -1, nplot, dtype=int)
+
+    if args.use_tex:
+        displaystyle = r'\displaystyle'
+    else:
+        displaystyle = r''
+    ylabel = r'$ ' + displaystyle + r'\frac{G}{G^\mathrm{norm}}$'
+
+    fig, ax, plots = lpd.create_figure(xlims=[0.15, 0.52], ylims=ylims, xlabel=r'$\tau T$', ylabel=ylabel, UseTex=args.use_tex)
+
+    ax.text(0.99, 0.99, r'$\sqrt{8\tau_\mathrm{F}} T=$ ' + '{0:.3f}'.format(flowradii[index]), ha='right', va='top', transform=ax.transAxes, bbox=lpd.labelboxstyle)
+
+    # loop over all the different data sets and plot settings
+    for tauTs, thisXX, thisXX_err, label, color, zorder in zip(tauT, XX, XX_err, labels, colors, zorders):
+        thisplot = ax.errorbar(tauTs, thisXX[index], thisXX_err[index], fmt='|', color=color, label=label, zorder=zorder + 2)
+        plots.append(thisplot)
+
+    ax.legend(handles=plots)
+    handles, labels = ax.get_legend_handles_labels()
+    leg = ax.legend(handles[::-1], labels[::-1], title="", loc="lower right", bbox_to_anchor=(1.01, 0.1), framealpha=0.5, handlelength=1, **lpd.leg_err_size(1, 0.3))
+    leg.set_zorder(-1)
+    # for line in leg.get_lines():
+    #     line.set_linewidth(4.0)
+    if index != 0:
+        ax.axvline(x=lpd.lower_tauT_limit_(flowradii[index]), **lpd.verticallinestyle)
+
+    return fig
+
+
+def apply_tree_level_imp(XX,nt, flowtimes, corr, flowaction, gaugeaction):
+    for i in range(XX.shape[0]):
+        flowtime = flowtimes[i]
+        for j in range(XX.shape[1]):
+            XX[i, j] = XX[i, j] / lpd.G_latt_LO_flow(j, flowtime, corr, nt, flowaction, gaugeaction) * nt**4
+    return XX
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--qcdtype', required=True, type=str)
+    parser.add_argument('--conftypes', nargs='*', type=str)
+    parser.add_argument('--continuum', type=str, help="path to continuum mean")
+    parser.add_argument('--continuum_err', type=str, help="path to continuum err")
+    parser.add_argument('--flowtimesT2', type=str, help="path to file that contains flowtimes*T^2")
+    parser.add_argument('--flow_index_range', default=(0, -1), type=int, nargs=2,
+                        help="which flow indices to consider (default considers all). useful if youre only interested in some speficic ones.")
+    parser.add_argument('--use_tex', default=True, action="store_false")
+    parser.add_argument('--nproc', default=20, type=int, help="number of processes for parallelization over flow times.")
+    parser.add_argument('--corr', choices=["EE", "BB"], required=True)
+    parser.add_argument('--basepath', type=str, default="../../data/merged/")  # TODO add help
+    parser.add_argument('--ylims', type=float, nargs=2, default=None)
+    parser.add_argument('--outputfolder', type=str, default=None)
+    parser.add_argument('--hide_cont', default=False, action="store_true")
+    args = parser.parse_args()
 
-    try:
-        qcdtype, fermions, temp, flowtype, corr, add_params = lpd.read_args(skip_conftype=True)
-        flowindex = add_params[0]
-        conftypes = add_params[1:]
-    except:
-        exit("Invalid Arguments. Usage: script.py <qcdtype> <corr> <flowindex> <conftype1> <conftype2> <...>, e.g. script.py quenched_1.50Tc_zeuthenFlow XX s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400")
+    fermions, temp, flowtype, gaugeaction, flowaction = lpd.parse_qcdtype(args.qcdtype)
 
-    i = int(flowindex) #create alias
-    flow_radius = numpy.loadtxt(lpd.get_merged_data_path(qcdtype,corr,conftypes[0])+"flowradii_"+conftypes[0]+".dat")
-    flowradius = flow_radius[i]
+    flowtimesT2 = numpy.loadtxt(args.flowtimesT2)
 
-    outputfolder = lpd.get_plot_path(qcdtype, corr, "")+"/lattice_effects/"
-
-    use_imp = True
-
-    UseTex = True
-    if UseTex:
-        displaystyle = r'\displaystyle'
-        ylabel = r'$ '+displaystyle+r'\frac{G^\mathrm{ latt }_{\tau_\mathrm{F}} (\tau)}{G_{\tau_\mathrm{F} = 0}^{\substack{ \text{\tiny  norm} \\[-0.4ex] \text{\tiny latt } } } (\tau)} $'
-    else:
-        ylabel = r'G'
-        displaystyle = r''
-        
-    fig, ax, plots = lpd.create_figure(xlims=[0.15,0.51], ylims=[2,4], xlabel=r'$\tau T$', ylabel=ylabel, xlabelpos=(0.95,0.05), ylabelpos=(0.03,0.98), UseTex = UseTex)
-
-    lpd.titlestyle.update(dict(y=0.95))
-    lpd.legendstyle.update(dict(loc="lower right", bbox_to_anchor=(1.01,0.1), framealpha=0.5, handlelength=1, handler_map={matplotlib.container.ErrorbarContainer: matplotlib.legend_handler.HandlerErrorbar(xerr_size=0.4)}))
-
-
-    ### load lattice data and interpolations
-    XX_data = []
+    # load lattice data and interpolations
     XX = []
     XX_err = []
     tauT = []
     Ntaus = []
-    for conftype in conftypes: 
-        inputfolder = lpd.get_merged_data_path(qcdtype,corr,conftype)
+    XX_int = []
+
+    for conftype in args.conftypes:
+
+        inputfolder = lpd.get_merged_data_path(args.qcdtype, args.corr, conftype, args.basepath)
         beta, ns, nt, nt_half = lpd.parse_conftype(conftype)
         Ntaus.append(nt)
-        XX_data.append(numpy.loadtxt(inputfolder+"/interpolations/"+corr+"_"+'{0:.3f}'.format(flow_radius[i])+"_interpolation.txt", unpack=True))
-        XX.append(numpy.loadtxt(inputfolder+"/"+corr+"_"+conftype+".dat"))
-        XX_err.append(numpy.loadtxt(inputfolder+"/"+corr+"_err_"+conftype+".dat"))
-        tauT.append(numpy.arange(1/nt,0.501,1/nt))
-        
-    labels = [r'$N_\tau = '+str(nt)+r'$' for nt in Ntaus] 
-    ### also load continuum extr if possible
-    try:
-        XX_data.append(numpy.loadtxt(lpd.get_merged_data_path(qcdtype,corr,"")+"/cont_extr/"+corr+"_"+'{0:.3f}'.format(flow_radius[i])+"_cont.txt", unpack=True))
-        labels.append(r'cont.')
-    except:
-        XX_data.append(None)
-        
-    ### some plot settings
-    colors=cm.tab10(numpy.linspace(0,1,10))
-    plots = []
-    ax.set_title(r'$\sqrt{8\tau_\mathrm{F}} T=$ '+'{0:.3f}'.format(flow_radius[i]), **lpd.titlestyle)
-    markers = ['s', 'o', 'D', 'H', 'h']
-    zorders = range(-30,0,4)
+        tmp = numpy.loadtxt(inputfolder + "/" + args.corr + "_" + conftype + ".dat")
+        tmp = apply_tree_level_imp(tmp, nt, flowtimesT2*nt**2, args.corr, flowaction, gaugeaction)
+        XX.append(tmp)
+        tmp = numpy.loadtxt(inputfolder + "/" + args.corr + "_err_" + conftype + ".dat")
+        tmp = numpy.fabs(apply_tree_level_imp(tmp, nt, flowtimesT2 * nt ** 2, args.corr, flowaction, gaugeaction))
+        XX_err.append(tmp)
 
-    ### loop over all the different data sets and plot settings
-    for XX_int, tauTs, XX, XX_err, label, color, marker, zorder in itertools.zip_longest(XX_data, tauT, XX, XX_err, labels, colors, markers, zorders):
-        lpd.plotstyle_add_point.update(dict(fmt=marker))
-        if tauTs is not None: #tauTs is None for the continuum limit
-            nt = int(len(tauTs)*2)
-        if XX_int is not None:
-            ### remove nans
-            columns = []
-            nan_array = numpy.isnan(XX_int[1])
-            for l,column in enumerate(XX_int):
-                columns.append(column[~nan_array])
-            ### plot original Data (XX, XX_err), interpolations (XX_int) and continuum extrapolation
-            if tauTs is None: #continuum plot
-                plots.append(ax.errorbar(columns[0], columns[1], columns[2], color=color, label=label, **lpd.plotstyle_add_point, zorder=zorder+1))
-                lpd.plotstyle_add_point.update(dict(fmt='-'))
-                ax.errorbar(columns[0], columns[1], columns[2], color=color, **lpd.plotstyle_add_point, zorder=zorder+1)
-            else: #interpolation plot
-                ax.errorbar(columns[0], columns[1], fmt = '-', lw = 0.5, mew=0, color=color, zorder=zorder+1)   
-                plots.append(ax.errorbar(tauTs, [val * lpd.improve_corr_factor(v, nt, i, use_imp) for v,val in enumerate(XX[i,:])], [val * lpd.improve_corr_factor(v, nt, i, use_imp) for v,val in enumerate(XX_err[i,:])], **lpd.plotstyle_add_point, color=color, label=label, zorder=zorder+2))
-    #if len(plots) < 4:
-        #exit("Error: need at least 4 lattices")
-    ax.legend(handles=plots)
-    handles, labels = ax.get_legend_handles_labels()
-    leg = ax.legend(handles[::-1], labels[::-1], title="", **lpd.legendstyle)
-    leg.set_zorder(-1)
-    for line in leg.get_lines():
-        line.set_linewidth(4.0)
-    if i != 0:
-        ax.axvline(x=lpd.lower_tauT_limit(flowradius), **lpd.verticallinestyle)
-    matplotlib.pyplot.tight_layout(0.2)
-    lpd.create_folder(outputfolder)
-    fig.savefig(outputfolder+"/"+corr+"_flow_"+'{0:.3f}'.format(flow_radius[i])+".pdf")#, **lpd.figurestyle) 
-    print("saved lattice effects plot", outputfolder+"/"+corr+"_flow_"+'{0:.3f}'.format(flow_radius[i])+".pdf") 
+        # TODO load interpolations.
+        # tmp = numpy.loadtxt(inputfolder + args.corr + "_" + args.conftype + "_interpolation_mean.npy")
+        # XX_int.append(tmp)
+
+        tauT.append(lpd.get_tauTs(nt))
+
+    labels = [r'$N_\tau = ' + str(nt) + r'$' for nt in Ntaus]
+
+    # add cont
+    if not args.hide_cont:
+        XX.append(numpy.loadtxt(args.continuum))
+        XX_err.append(numpy.loadtxt(args.continuum_err))
+        tauT.append(lpd.get_tauTs(Ntaus[-1]))
+        labels.append(r'cont.')
+
+    if args.flow_index_range == (0, -1):
+        indices = range(0, len(flowtimesT2))
+    else:
+        indices = range(*args.flow_index_range)
+
+    matplotlib.rcParams['figure.max_open_warning'] = 0  # suppress warning due to possibly large number of figures...
+    figs = lpd.parallel_function_eval(plot_single_flowtime, indices, args.nproc, numpy.sqrt(8*flowtimesT2), args, tauT, XX, XX_err, labels, args.ylims)
+
+    # save figures to pdf
+    print("save figures...")
+    lpd.set_rc_params()  # for some reason we need to repeat this here...
+    if args.outputfolder is None:
+        args.outputfolder = lpd.get_plot_path(args.qcdtype, args.corr, "")
+    lpd.create_folder(args.outputfolder)
+    with PdfPages(args.outputfolder + "/" + args.corr + "_latt_effects.pdf") as pdf:
+        for fig in figs:
+            pdf.savefig(fig)
+            matplotlib.pyplot.close(fig)
+
 
 if __name__ == '__main__':
+    lpd.print_script_call()
     main()
+    lpd.save_script_call()
--- plotting/2_plot_lateffects.sh
+++ plotting/2_plot_lateffects.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
-rm -f /work/home/altenkort/work/EE_correlator/plots/quenched/lattice_effects/*
-rm -f /home/altenkort/.cache/matplotlib/tex.cache/*matplotlib-lock
 
-sbatchscript=$(cat <<EOF
-#! /bin/bash
-#SBATCH --job-name=plot_interpolations
-#SBATCH --output=./%x.out
-#SBATCH --error=./%x.err
-#SBATCH --time=24:00:00
-#SBATCH --partition=devel_cpu
-#SBATCH --ntasks=1
-##SBATCH --array=0-133
+qcdtype=$1
 
-export PATH=\$PATH:/usr/local/texlive/2020/bin/x86_64-linux
-parameters=()
-for i in {0..133} ; do
-    srun --exclusive python -u 2_plot_lateffects.py \$i
+# TODO add BB
+
+if [ -z "$qcdtype" ] ; then
+    echo "Usage: $0 qcdtype"
+    echo "choices for qcdtype: quenched_1.50Tc_zeuthenFlow hisq_ms5_zeuthenFlow"
+    exit
+fi
+
+ncpu=20
+
+if [ "$qcdtype" == quenched_1.50Tc_zeuthenFlow ] ; then
+    arr_conftypes=("s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400")
+    continuum="../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont.dat"
+    continuum_err="../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont_err.dat"
+    flowtimesT2="../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont_flowtimesT2.dat"
+    ylims="2 4"
+elif [ "$qcdtype" == hisq_ms5_zeuthenFlow ] ; then
+
+    # TODO make plots for hisq
+    arr_conftypes=(\
+    "s064t20_b0803600 s064t22_b0814700 s096t24_b0824900_m002022_m01011"
+    "s064t20_b0785700 s064t24_b0806800 s096t28_b0824900_m002022_m01011"
+    "s064t20_b0770400 s064t24_b0791300 s096t32_b0824900_m002022_m01011"
+    "s064t20_b0757000 s064t24_b0777700 s096t36_b0824900_m002022_m01011")
+    arr_output_suffix=("T296" "T251" "T220" "T196")
+    ylims_arr=("3.5 9.9" "3.5 9.9" "3.5 9.9" "3.5 9.9")
+    basepath="../../data/merged/hisq_ms5_zeuthenFlow/EE"
+fi
+
+
+for idx in "${!arr_conftypes[@]}" ; do
+
+    if [ "$qcdtype" == hisq_ms5_zeuthenFlow ] ; then
+        temp=${arr_output_suffix[idx]}
+        flowtimesT2="${basepath}/${temp}/cont_extr/EE_cont_flowtimesT2.dat"
+        continuum="${basepath}/${temp}/cont_extr/EE_cont.dat"
+        continuum_err="${basepath}/${temp}/cont_extr/EE_cont_err.dat"
+        outputfolder="--outputfolder ../../plots/$qcdtype/EE/$temp/"
+        ylims=${ylims_arr[idx]}
+        add_args="--hide_cont"
+    fi
+
+./2_plot_lateffects.py \
+--corr EE --qcdtype $qcdtype \
+--conftypes ${arr_conftypes[idx]} \
+--continuum ${continuum} \
+--continuum_err ${continuum_err} \
+--flowtimesT2 ${flowtimesT2} \
+--nproc $ncpu \
+--ylims $ylims \
+$outputfolder \
+$add_args
+
 done
-#parameters+=("\$i")
-#\${parameters[\$SLURM_ARRAY_TASK_ID]}
-EOF
-)
-sbatch <(cat <<< "$sbatchscript")
+
+
