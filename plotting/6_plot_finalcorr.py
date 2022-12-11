6e847124042eabe5f6ff561583d4bc45d3539557
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-12-11 15:20:28 +0100

    update final corr plot with new styles and conventions

--- plotting/6_plot_finalcorr.py
+++ plotting/6_plot_finalcorr.py
@@ -1,48 +1,45 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 
 import numpy
-import matplotlib
 import lib_process_data as lpd
+import argparse
+
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_flow', type=str, required=True)
+    parser.add_argument('--input_multilvl', type=str, required=True)
+    parser.add_argument('--input_multilvl2', type=str, default="")
+    parser.add_argument('--outputfolder', type=str, default="../../plots/")
+    args = parser.parse_args()
 
-    qcdtype, fermions, temp, flowtype, corr, add_params = lpd.read_args(skip_conftype=True)
+    EE_final = numpy.loadtxt(args.input_flow, unpack=True)  #
+    # EE_cont_2015 = numpy.loadtxt(args.input_multilvl)  # "../../data_merged/quenched/multi-level_2015/cont_thomas.dat"
+    EE_cont_2015_new = numpy.loadtxt(args.input_multilvl)  #  inputfolder + "/multi-level_2015/EE_2015_new.txt")
+    EE_cont_2015_new_2022 = None
+    try:
+        EE_cont_2015_new_2022 = numpy.loadtxt(args.input_multilvl2)
+    except OSError:
+        print("skip multilvl2")
 
-    inputfolder = lpd.get_merged_data_path(qcdtype,corr,"")
+    fig, ax, plots = lpd.create_figure(xlims=[0.05, 0.52], ylims=[1.5, 3.8], xlabel=r'$\tau T$',
+                                       ylabel=r'$\displaystyle \frac{G}{G^{\mathrm{norm}}}$')
 
-    #input
-    EE_final = numpy.loadtxt(inputfolder+corr+"_final.txt", unpack=True)
-    EE_cont_2015 = numpy.loadtxt("../../data_merged/quenched/multi-level_2015/cont_thomas.dat")
-    EE_cont_2015_new = numpy.loadtxt(inputfolder+"/multi-level_2015/EE_2015_new.txt")
+    ax.errorbar(EE_cont_2015_new[2:, 0], EE_cont_2015_new[2:, 1], EE_cont_2015_new[2:, 2],
+                             label=r'Multi-level method', fmt='|')  # +"\n"+r'w/ $2016$ renorm. update'))#\beta_g (\mathcal{Z}_\mathrm{pert} -1)/6 \approx 0.138
+    if EE_cont_2015_new_2022 is not None:
+        ax.errorbar(EE_cont_2015_new_2022[2:, 0], EE_cont_2015_new_2022[2:, 1], EE_cont_2015_new_2022[2:, 2],
+                label=r'Multi-level method', fmt='|')
+    ax.errorbar(EE_final[0], EE_final[1], EE_final[2], fmt='|', label="Gradient flow method")
+    # plots.append(ax.errorbar(lpd.tauT_30_ext[lpd.start:lpd.end], [k[1] for k in EE_final_old], [k[2] for k in EE_final_old], **lpd.plotstyle_add_point, label="Gradient flow method old", color=matplotlib.cm.gnuplot(0.8)))
+    # plots.append(ax.errorbar(EE_cont_2015[:,0], EE_cont_2015[:,1], EE_cont_2015[:,2], 's-', label=r'Multi-level method$^1$'+"\n"+r'2015'))#\beta_g (\mathcal{Z}_\mathrm{pert} -1)/6 = 0.079
 
-    #output
-    #   lpd.outputfolder+"/EE_aeq0_teq0.pdf"
+    ax.legend(loc='lower right', bbox_to_anchor=(1, 0.1), **lpd.leg_err_size())
+    fig.savefig(args.outputfolder + "/EE_flowVSmultilvl.pdf")
+    print("saved final corr plot", args.outputfolder + "/EE_flowVSmultilvl.pdf")
 
 
-    fig, ax, plots = lpd.create_figure(xlims=[0.05,0.51], ylims=[1.5,3.8], xlabel=r'$\tau T$', ylabel=r'$\displaystyle \frac{G^\mathrm{cont }(\tau) }{G^{\substack{ \text{\tiny  norm} \\[-0.5ex] \text{\tiny cont } } } (\tau) }$ ', xlabelpos=(0.95,0.07), ylabelpos=(0.03,0.98)) # 
-    lpd.legendstyle.update(dict(loc='lower right', bbox_to_anchor=(1,0.1), labelspacing=0.4, handler_map={matplotlib.container.ErrorbarContainer: matplotlib.legend_handler.HandlerErrorbar(xerr_size=0.5)} ))
-
-    lpd.plotstyle_add_point.update(dict(fmt='s-'))
-    plots.append(ax.errorbar(EE_final[0], EE_final[1], EE_final[2], **lpd.plotstyle_add_point, label="Gradient flow method")) #color=matplotlib.cm.gnuplot(0.5)
-    #plots.append(ax.errorbar(EE_final_btstrp[0], EE_final_btstrp[1], EE_final_btstrp[2], **lpd.plotstyle_add_point, label="Gradient flow method btstrp"))
-    #plots.append(ax.errorbar(lpd.tauT_30_ext[lpd.start:lpd.end], [k[1] for k in EE_final_old], [k[2] for k in EE_final_old], **lpd.plotstyle_add_point, label="Gradient flow method old", color=matplotlib.cm.gnuplot(0.8)))
-    lpd.plotstyle_add_point.update(dict(fmt='o-'))
-    plots.append(ax.errorbar(EE_cont_2015_new[2:,0], EE_cont_2015_new[2:,1], EE_cont_2015_new[2:,2], **lpd.plotstyle_add_point, label=r'Multi-level method'))#+"\n"+r'w/ $2016$ renorm. update'))#\beta_g (\mathcal{Z}_\mathrm{pert} -1)/6 \approx 0.138
-    lpd.plotstyle_add_point.update(dict(fmt='s-'))
-    #plots.append(ax.errorbar(EE_cont_2015[:,0], EE_cont_2015[:,1], EE_cont_2015[:,2], **lpd.plotstyle_add_point, label=r'Multi-level method$^1$'+"\n"+r'2015'))#\beta_g (\mathcal{Z}_\mathrm{pert} -1)/6 = 0.079 
-
-    leg = ax.legend(handles=plots, title="", **lpd.legendstyle)
-    leg.set_zorder(-1000)
-    matplotlib.pyplot.tight_layout(0)
-    ### change first xtick label
-    fig.canvas.draw()
-    ticks=ax.get_xticks().tolist()
-    ticks = ['{0:.2f}'.format(x) for x in ticks]
-    ticks[-2]=0.5
-    #ticks[0]=0
-    ax.set_xticklabels(ticks)
-    fig.savefig(lpd.outputfolder+"/EE_aeq0_teq0.pdf") 
-    print("saved final corr plot", lpd.outputfolder+"/EE_aeq0_teq0.pdf")
-    
 if __name__ == '__main__':
+    lpd.print_script_call()
     main()
+    lpd.save_script_call()
