3bbcd1a124c7ac147a9635768c9347f74ebb4783
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-12-25 14:27:39 +0100

    make calc_flowtimes.py way less confusing

--- process_data/calc_flowtimes.py
+++ process_data/calc_flowtimes.py
@@ -1,8 +1,9 @@
-#!/usr/local/bin/python
+#!/usr/env/bin python3
 import numpy as np
+import lib_process_data as lpd
+from matplotlib import pyplot
 
 
-# forwards
 def conv_tF_to_tauFT2(tF, Ntau):
     tF = np.asarray(tF)
     return tF / Ntau ** 2
@@ -17,7 +18,10 @@ def conv_tF_to_sqrt8tauFT(tF, Ntau):
     return conv_tauFT2_to_sqrt8tauFT(conv_tF_to_tauFT2(tF, Ntau))
 
 
-# backwards
+def conv_tF_to_sqrt8tF(tF):
+    return np.sqrt(8*tF)
+
+
 def conv_sqrt8tauFT_to_tauFT2(sqrt8tauFT):
     sqrt8tauFT = np.asarray(sqrt8tauFT)
     return sqrt8tauFT ** 2 / 8
@@ -33,47 +37,45 @@ def conv_sqrt8tauFT_to_tF(sqrt8tauFT, Ntau):
     return conv_tauFT2_to_tF(conv_sqrt8tauFT_to_tauFT2(sqrt8tauFT), Ntau)
 
 
+def conv_sqrt8tF_to_tF(sqrt8tF):
+    return sqrt8tF**2/8
+
+
 def stepsizes(values):
     return np.asarray([values[i + 1] - val for i, val in enumerate(values) if values[i] != values[-1]])
 
 
-# proposed stepsize
+def get_flowtimes(Ntau, max_sqrt8tF=None, first_step_in_sqrt8tF=0.1, stepsize_increment_rate=1.03, max_stepsize_in_tF=0.15):
+    # this creates flowtime steps that are similar (but with slighlty smaller stepsizes) to what the adaptive stepsize algorithm would
+    # produce with accuracy=1e-05 and initial stepsize=1e-04
+    if max_sqrt8tF is None:
+        max_sqrt8tF = Ntau/2
 
-Ntau = 64
+    proposed_tF = [0,  0.0001, conv_sqrt8tF_to_tF(first_step_in_sqrt8tF)]
+    current_tF = proposed_tF[-1]
+    current_stepsize_in_tF = conv_sqrt8tF_to_tF(first_step_in_sqrt8tF)
+    while current_tF < conv_sqrt8tF_to_tF(max_sqrt8tF):
+        # update stepsize
+        if current_stepsize_in_tF < max_stepsize_in_tF:
+            current_stepsize_in_tF = conv_sqrt8tF_to_tF(conv_tF_to_sqrt8tF(current_stepsize_in_tF)*stepsize_increment_rate)
+            if current_stepsize_in_tF > max_stepsize_in_tF:
+                current_stepsize_in_tF = max_stepsize_in_tF
+        current_tF += current_stepsize_in_tF
+        proposed_tF.append(float(np.format_float_positional(current_tF, precision=4, unique=False, fractional=False, trim='k')))  # round to 4 significant digits.
 
-max_stepsize = 0.075  # 0.15
-max_sqrt8tauFT = 0.5  # 0.3
+    # remove lastly added tF, which is too large
+    tF = np.asarray([*proposed_tF[:-1], conv_sqrt8tF_to_tF(max_sqrt8tF)])
 
-under_150 = 0
-# proposed_sqrt8tauFT = [0,conv_tF_to_sqrt8tauFT(0.001,Ntau)]
-proposed_sqrt8tauFT = [0, 0.0001]  # 0.001
-current_sqrt8tauFT = proposed_sqrt8tauFT[-1]  # inital step
-current_stepsize = 0.0004  # 0.001.  start stepsize (after first step)
-
-# while conv_sqrt8tauFT_to_tF(proposed_sqrt8tauFT[-1],Ntau) - conv_sqrt8tauFT_to_tF(proposed_sqrt8tauFT[-2],Ntau) < max_stepsize and current_sqrt8tauFT < max_sqrt8tauFT:
-while current_sqrt8tauFT <= max_sqrt8tauFT:  # use this and fixed_stepsize=0.15 to get the output that you can put into the gradientFlow
-    current_sqrt8tauFT += current_stepsize
-    proposed_sqrt8tauFT.append(current_sqrt8tauFT)
-    current_stepsize *= 1.01
-    if current_sqrt8tauFT < 0.150:
-        under_150 += 1
-
-proposed_sqrt8tauFT = np.asarray(proposed_sqrt8tauFT[:-1])
-proposed_sqrt8tauFT = np.append(proposed_sqrt8tauFT, max_sqrt8tauFT)
-proposed_tF = conv_sqrt8tauFT_to_tF(proposed_sqrt8tauFT, Ntau)
-
-while proposed_tF[-1] < conv_sqrt8tauFT_to_tF(max_sqrt8tauFT, Ntau):
-    print(proposed_tF[-1], conv_sqrt8tauFT_to_tF(0.5,Ntau))
-    proposed_tF = np.append(proposed_tF, proposed_tF[-1]+max_stepsize)
+    return tF
 
 
-proposed_sqrt8tauFT = conv_tF_to_sqrt8tauFT(proposed_tF[:-1],Ntau)
-proposed_sqrt8tauFT = np.append(proposed_sqrt8tauFT, max_sqrt8tauFT)
+Ntau = 20
+tF = get_flowtimes(20)
+fig, ax, _ = lpd.create_figure()
+
+ax.set_yscale('log')
+ax.errorbar(range(len(tF)), tF, fmt='x')
+pyplot.show()
 
 
-print(proposed_sqrt8tauFT)
-print(proposed_tF)
-
-np.savetxt("flowradii_fine.dat", proposed_sqrt8tauFT, fmt='%.8f')
-np.savetxt("flowtimes_fine.dat", proposed_tF, fmt='%.8f')  # newline=' ',
-
+np.savetxt("flowtimes_Nt"+str(Ntau)+".dat", tF, fmt='%.8f')
