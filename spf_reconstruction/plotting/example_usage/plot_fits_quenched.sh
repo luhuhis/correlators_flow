1ba36f9ce67af12a6780e44283b5a3fbd6e387c4
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-12-30 17:54:36 +0100

    update example script for quenched fits

--- spf_reconstruction/plotting/example_usage/plot_fits_quenched.sh
+++ spf_reconstruction/plotting/example_usage/plot_fits_quenched.sh
@@ -3,27 +3,27 @@
 
 # EE quenched 1.5 Tc
 
-#nsamples=1000
+finalsamples=10000
 highsamples=1000
-#lowsamples=100
+lowsamples=100
 
 labels_and_models=(
-"max NLO"         "max_NLO_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
-"max LO"          "max_LO_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
-"smax NLO"        "smax_NLO_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
-"smax LO"         "smax_LO_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
-#"line NLO"        "line_wIR1.0_wUV3.14_NLO_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
-#"line LO"         "line_wIR1.0_wUV3.14_LO_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
-#"fourier NLO 1"   "fourier_NLO_s1_beta_1_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
-#"fourier LO 1"    "fourier_LO_s1_alpha_1_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
-"fourier NLO 2"  "fourier_NLO_s1_beta_1_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
-"fourier LO 2"   "fourier_LO_s1_alpha_1_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
-"fourier NLO 3"   "fourier_NLO_s1_beta_2_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
-"fourier LO 3"    "fourier_LO_s1_alpha_2_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
-#"fourier NLO 4"   fourier_NLO_s1_beta_3_Nf0_T0.472_minpiT_wopt_${lowsamples}smpls_tauTgtr0.24_final
-#"fourier LO 4"    fourier_LO_s1_alpha_3_Nf0_T0.472_minpiT_w1_${lowsamples}smpls_tauTgtr0.24_final
-#"fourier NLO 5"   fourier_NLO_s1_beta_4_Nf0_T0.472_minpiT_wopt_${lowsamples}smpls_tauTgtr0.24_final
-#"fourier LO 5"    fourier_LO_s1_alpha_4_Nf0_T0.472_minpiT_w1_${lowsamples}smpls_tauTgtr0.24_final
+'$\text{max}_\text{NLO}$'                   "max_NLO_Nf0_T0.472_minpiT_wopt_${finalsamples}smpls_tauTgtr0.24_final"
+'$\text{max}_\text{LO\hphantom{N}}$'        "max_LO_Nf0_T0.472_minpiT_w1_${finalsamples}smpls_tauTgtr0.24_final"
+'$\text{smax}_\text{NLO}$'                  "smax_NLO_Nf0_T0.472_minpiT_wopt_${finalsamples}smpls_tauTgtr0.24_final"
+'$\text{smax}_\text{LO\hphantom{N}}$'       "smax_LO_Nf0_T0.472_minpiT_w1_${finalsamples}smpls_tauTgtr0.24_final"
+'$\text{plaw}_\text{NLO}$'                  "plaw_wIR1.0_wUV3.14_NLO_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
+'$\text{plaw}_\text{LO\hphantom{N}}$'       "plaw_wIR1.0_wUV3.14_LO_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
+'$\text{trig1}_\text{NLO}$'                 "fourier_NLO_s1_beta_1_Nf0_T0.472_minpiT_wopt_${finalsamples}smpls_tauTgtr0.24_final"
+'$\text{trig1}_\text{LO\hphantom{N}}$'      "fourier_LO_s1_alpha_1_Nf0_T0.472_minpiT_w1_${finalsamples}smpls_tauTgtr0.24_final"
+'$\text{trig2}_\text{NLO}$'                 "fourier_NLO_s1_beta_2_Nf0_T0.472_minpiT_wopt_${finalsamples}smpls_tauTgtr0.24_final"
+'$\text{trig2}_\text{LO\hphantom{N}}$'      "fourier_LO_s1_alpha_2_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
+#'$\text{trig3}_\text{NLO}$'                 "fourier_NLO_s1_beta_3_Nf0_T0.472_minpiT_wopt_${highsamples}smpls_tauTgtr0.24_final"
+#'$\text{trig3}_\text{LO\hphantom{N}}$'      "fourier_LO_s1_alpha_3_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
+#'$\text{trig4}_\text{NLO}$'                 "fourier_NLO_s1_beta_4_Nf0_T0.472_minpiT_wopt_${lowsamples}smpls_tauTgtr0.24_final"
+#'$\text{trig4}_\text{LO\hphantom{N}}$'      "fourier_LO_s1_alpha_4_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
+#'$\text{trig5}_\text{NLO}$'                 "fourier_NLO_s1_beta_5_Nf0_T0.472_minpiT_wopt_${lowsamples}smpls_tauTgtr0.24_final"
+#'$\text{trig5}_\text{LO\hphantom{N}}$'      "fourier_LO_s1_alpha_5_Nf0_T0.472_minpiT_w1_${highsamples}smpls_tauTgtr0.24_final"
 )
 
 models=()
@@ -39,14 +39,16 @@ done
 
 plot_kappa() {
     ../plot_kappa.py \
---xlims 0 4.2 \
+--xlims -0.15 4.15 \
+--xticks 0 1 2 3 4 \
 --model_ids "${models[@]}" \
 --labels "${labels[@]}" \
 --basepath ~/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/spf/ \
 --outputpath ~/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/EE/ \
 --suffix _quenched_1.5Tc \
 --corr EE \
---scale_error_by_chisqdof
+--scale_error_by_chisqdof \
+--figsize 7 5
 }
 
 plot_fitcorr() {
@@ -68,7 +70,7 @@ plot_spfs(){
 --suffix _quenched_1.5Tc \
 --corr EE \
 --plot_spf_err \
---ylims 0.8 150
+--ylims 1 150
 }
 
 plot_spfs
