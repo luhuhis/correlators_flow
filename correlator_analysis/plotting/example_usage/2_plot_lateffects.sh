f02fe4ac75c09400d524f0b44c31ff98cb063958
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-01-06 15:54:36 +0100

    update paths in lat effects plot

--- correlator_analysis/plotting/example_usage/2_plot_lateffects.sh
+++ correlator_analysis/plotting/example_usage/2_plot_lateffects.sh
@@ -14,9 +14,9 @@ ncpu=20
 
 if [ "$qcdtype" == quenched_1.50Tc_zeuthenFlow ] ; then
     arr_conftypes=("s080t20_b0703500 s096t24_b0719200 s120t30_b0739400 s144t36_b0754400")
-    continuum="../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont.dat"
-    continuum_err="../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont_err.dat"
-    flowtimesT2="../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont_flowtimesT2.dat"
+    continuum="../../../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont.dat"
+    continuum_err="../../../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont_err.dat"
+    flowtimesT2="../../../../data/merged/quenched_1.50Tc_zeuthenFlow/EE/cont_extr/EE_cont_flowtimesT2.dat"
     ylims="2 4.1"
     add_args="--hide_cont --xlims 0 0.52 --lower_limit_text_pos 3.5"
 elif [ "$qcdtype" == hisq_ms5_zeuthenFlow ] ; then
@@ -29,7 +29,7 @@ elif [ "$qcdtype" == hisq_ms5_zeuthenFlow ] ; then
     "s064t20_b0757000 s064t24_b0777700 s096t36_b0824900_m002022_m01011")
     arr_output_suffix=("T296" "T251" "T220" "T196")
     ylims_arr=("3.5 10.5" "3.5 10.5" "3.5 10.5" "3.5 10.5")
-    basepath="../../data/merged/hisq_ms5_zeuthenFlow/EE"
+    basepath="../../../../data/merged/hisq_ms5_zeuthenFlow/EE"
     add_args="--hide_cont --lower_limit_text_pos 8"
 fi
 
@@ -41,11 +41,11 @@ for idx in "${!arr_conftypes[@]}" ; do
         flowtimesT2="${basepath}/${temp}/cont_extr/EE_cont_flowtimesT2.dat"
         continuum="${basepath}/${temp}/cont_extr/EE_cont.dat"
         continuum_err="${basepath}/${temp}/cont_extr/EE_cont_err.dat"
-        outputfolder="--outputfolder ../../plots/$qcdtype/EE/$temp/"
+        outputfolder="--outputfolder ../../../../plots/$qcdtype/EE/$temp/"
         ylims=${ylims_arr[idx]}
     fi
 
-./2_plot_lateffects.py \
+../2_plot_lateffects.py \
 --corr EE --qcdtype $qcdtype \
 --conftypes ${arr_conftypes[idx]} \
 --continuum ${continuum} \
