6ec1ef0ea38fea9e0b8471d8c27277b623de1cd1
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-01-06 15:59:50 +0100

    add custom x and y ticks to fit corr plot

--- spf_reconstruction/plotting/plot_fitcorr.py
+++ spf_reconstruction/plotting/plot_fitcorr.py
@@ -40,7 +40,9 @@ def parse_args():
     parser.add_argument('--model_ids', type=str, nargs='*')
 
     parser.add_argument('--xlims', nargs=2, type=float, default=(0.23, 0.54))
-    parser.add_argument('--ylims', nargs=2, type=float, default=(0.75, 1.25))
+    parser.add_argument('--ylims', nargs=2, type=float, default=(0.75, 1.15))
+    parser.add_argument('--yticks', nargs='*', default="auto")
+    parser.add_argument('--xticks', nargs='*', default="auto")
     parser.add_argument('--labels', nargs='*', type=str)
     parser.add_argument('--colors', nargs='*')
 
@@ -67,12 +69,20 @@ def main():
     fig, ax, plots = lpd.create_figure(xlabel=r'$\tau T$', ylabel=r'$\displaystyle \frac{G^\text{model}}{G}$', xlims=args.xlims, ylims=args.ylims)
 
     if args.colors is None:
-        args.colors = [lpd.get_discrete_color(i) for i in range(nfiles)]
+        args.colors = [lpd.get_discrete_color(int(i/2)) for i in range(nfiles)]
 
     for i in range(nfiles):
         if not numpy.isnan(xdata[i]).any():
-            ax.errorbar(xdata[i] + i * 0.003, ydata[i], yerr=[errorsleft[i], errorsright[i]], fmt='|', color=args.colors[i], label=args.labels[i])
+            ax.errorbar(xdata[i] + i * 0.002, ydata[i], yerr=[errorsleft[i], errorsright[i]],
+                        fmt='|', markersize=0, color=args.colors[i] if i % 2 == 0 else lpd.lighten_color(args.colors[i], 0.5), label=args.labels[i])
+            # ax.errorbar(xdata[i] + i * 0.002, ydata[i],
+            #             fmt='-', markersize=0, color=args.colors[i] if i % 2 == 0 else lpd.lighten_color(args.colors[i], 0.5), zorder=-100)
     ax.axhline(y=1, color='k', dashes=(2, 1))
+
+    if args.yticks != "auto":
+        ax.set_yticks([float(i) for i in args.yticks])
+    if args.xticks != "auto":
+        ax.set_xticks([float(i) for i in args.xticks])
 
     ax.legend(title="model", handlelength=1, loc="lower left", bbox_to_anchor=(0, 0), ncols=2, **lpd.leg_err_size(1, 0.3))
 
