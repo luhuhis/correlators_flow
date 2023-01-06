ba5f90dd4ffff99839d45b9fed415cee5d8e3b4d
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-01-06 15:57:10 +0100

    add BB spf reconstruct jobs

--- spf_reconstruction/model_fitting/example_usage/spf_reconstruct.sh
+++ spf_reconstruction/model_fitting/example_usage/spf_reconstruct.sh
@@ -9,18 +9,18 @@ set_UV_params(){
 
 set_models(){
     models=(
-        "--model max $LO"
-        "--model max $NLO"
-        "--model smax $LO"
-        "--model smax $NLO"
-        "--model plaw $LO --OmegaByT_IR 1 --OmegaByT_UV $OmegaByT_UV"
-        "--model plaw $NLO --OmegaByT_IR 1 --OmegaByT_UV $OmegaByT_UV"
-        "--model fourier $LO --mu alpha --nmax 1"
-        "--model fourier $NLO --mu beta --nmax 1"
-        "--model fourier $LO --mu alpha --nmax 2"
-        "--model fourier $NLO --mu beta --nmax 2"
-        "--model fourier $LO --mu alpha --nmax 3"
-        "--model fourier $NLO --mu beta --nmax 3"
+#        "--model max $LO"
+#        "--model max $NLO"
+#        "--model smax $LO"
+#        "--model smax $NLO"
+#        "--model plaw $LO --OmegaByT_IR 1 --OmegaByT_UV $OmegaByT_UV"
+#        "--model plaw $NLO --OmegaByT_IR 1 --OmegaByT_UV $OmegaByT_UV"
+        "--model trig $LO --mu alpha --nmax 1 --prevent_overfitting "
+        "--model trig $NLO --mu beta --nmax 1 --prevent_overfitting"
+        "--model trig $LO --mu alpha --nmax 2 --prevent_overfitting"
+        "--model trig $NLO --mu beta --nmax 2 --prevent_overfitting"
+#        "--model trig $LO --mu alpha --nmax 3 --prevent_overfitting"
+#        "--model trig $NLO --mu beta --nmax 3 --prevent_overfitting"
 #        "--model fourier $LO --mu alpha --nmax 4 --prevent_overfitting"
 #        "--model fourier $NLO --mu beta --nmax 4 --prevent_overfitting"
 #        "--model fourier $LO --mu alpha --nmax 5 --prevent_overfitting"
@@ -28,18 +28,39 @@ set_models(){
     )
 }
 
-submit_quenched(){
+submit_quenched_EE(){
     minscale=piT
     OmegaByT_UV=3.14
     set_UV_params
     set_models
 
-    nsamples=10000
+    nsamples=1000
     for i in "${!models[@]}" ; do
          spfbatch ../spf_reconstruct.py \
             --output_path /work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/spf/ \
             --add_suffix final \
             --input_corr /work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/EE_flow_extr.npy \
+            --min_tauT 0.24 \
+            --nproc 4 \
+            --T_in_GeV 0.472 \
+            --Nf 0 \
+            --nsamples $nsamples \
+            ${models[i]}
+    done
+}
+
+submit_quenched_BB(){
+    minscale=piT
+    OmegaByT_UV=3.14
+    set_UV_params
+    set_models
+
+    nsamples=100
+    for i in "${!models[@]}" ; do
+         spfbatch ../spf_reconstruct.py \
+            --output_path /work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/BB/spf/ \
+            --add_suffix final \
+            --input_corr /work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/BB/BB_flow_extr.npy \
             --min_tauT 0.24 \
             --nproc 4 \
             --T_in_GeV 0.472 \
@@ -76,8 +97,6 @@ submit_hisq(){
     done
 }
 
-
-
-
-#submit_quenched
+#submit_quenched_EE
+#submit_quenched_BB
 submit_hisq
