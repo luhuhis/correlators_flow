c044259307ed8d4efbfb2035e8452b42b5ce7c47
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-01-19 16:24:03 +0100

    move coupling plot script

--- spf_reconstruction/plot_fits/example_usage/plot_UV_spfs.sh
+++ spf_reconstruction/plot_fits/example_usage/plot_UV_spfs.sh
@@ -5,18 +5,29 @@ plot_quenched(){
     basepath="/work/home/altenkort/work/correlators_flow/data/merged/UV_spfs"
 
     labels_and_models=(
-#        "LO_piT_1" "${basepath}/SPF_LO_Nf0_0.472_piT_1_hmax.npy"
-        "LO_opt_1" "${basepath}/SPF_LO_Nf0_0.472_opt_1_hmax.npy"
+#        "EE_LO_piT_1" "${basepath}/SPF_LO_Nf0_0.472_piT_1_hmax.npy"
+        '$N_f=0, T=1.5T_c$' "dummy"
+        '$\mu=\mathrm{max}(\mu_\mathrm{eff}, 1)$' "${basepath}/SPF_LO_Nf0_0.472_eff_1_hmax.npy"
 #        "LO_2piT_1" "${basepath}/SPF_LO_Nf0_0.472_2piT_1_hmax.npy"
 #        "NLO_piT_1" "${basepath}/SPF_NLO_Nf0_0.472_piT_1_hmax.npy"
-#        "NLO_opt_1" "${basepath}/SPF_NLO_Nf0_0.472_opt_1_hmax.npy"
+#        "NLO_opt_1" "${basepath}/SPF_NLO_Nf0_0.472_eff_1_hmax.npy"
 #        "NLO_2piT_1" "${basepath}/SPF_NLO_Nf0_0.472_2piT_1_hmax.npy"
 #        "LO_piT_opt" "${basepath}/SPF_LO_Nf0_0.472_piT_opt_hmax.npy"
-#        "LO_opt_opt" "${basepath}/SPF_LO_Nf0_0.472_opt_opt_hmax.npy"
+#        "LO_opt_opt" "${basepath}/SPF_LO_Nf0_0.472_eff_opt_hmax.npy"
 #        "LO_2piT_opt" "${basepath}/SPF_LO_Nf0_0.472_2piT_opt_hmax.npy"
-#        "NLO_piT_opt" "${basepath}/SPF_NLO_Nf0_0.472_piT_opt_hmax.npy"
-        "NLO_opt_opt" "${basepath}/SPF_NLO_Nf0_0.472_opt_opt_hmax.npy"
+#        "EE_NLO_piT_opt" "${basepath}/SPF_NLO_Nf0_0.472_piT_opt_hmax.npy"
+#        "EE_LO_eff_opt" "${basepath}/SPF_LO_Nf0_0.472_eff_opt_hmax.npy"
+        '$\mu=\mathrm{max}(\mu_\mathrm{eff}, \mu_\mathrm{opt})$' "${basepath}/SPF_NLO_Nf0_0.472_eff_opt_hmax.npy"
+        '$\mu=\mathrm{max}(\mu_\mathrm{eff}, \mu_\mathrm{opt,B})$' "${basepath}/SPF_LO_Nf0_0.472_eff_optBB_hmax.npy"
+#        "BB_(N)LO_piT" "${basepath}/SPF_LO_Nf0_0.472_piT_optBB_hmax.npy"
+#        "BB_(N)LO_piT" "${basepath}/SPF_LO_Nf0_0.472_piT_optBB_hmax.npy"
+#        "BB_(N)LO_eff_piT" "${basepath}/SPF_LO_Nf0_0.472_eff_optBBpiT_hmax.npy"
+#        "BB_(N)LO_piT_piT" "${basepath}/SPF_LO_Nf0_0.472_piT_optBBpiT_hmax.npy"
 #        "NLO_2piT_opt" "${basepath}/SPF_NLO_Nf0_0.472_2piT_opt_hmax.npy"
+        ' ' "dummy"
+        '$N_f=3, T=1.6T_c$' "dummy"
+        '$\mu=\mathrm{max}(\mu_\mathrm{eff}, 1)$' "${basepath}/SPF_LO_Nf3_0.251_eff_1_hmax.npy"
+        '$\mu=\mathrm{max}(\mu_\mathrm{eff}, \mu_\mathrm{opt})$' "${basepath}/SPF_NLO_Nf3_0.251_eff_opt_hmax.npy"
     )
 
     models=()
@@ -30,15 +41,15 @@ plot_quenched(){
     done
 
     ../plot_UV_spf.py \
-        --xlims 0.01 1000 \
-        --ylims 0 10 \
-        --T_in_GeV 0.472 --Nf 0 \
+        --xlims 0.1 1500 \
+        --ylims 0 4.5 \
         --PhiUV_files "${models[@]}" \
         --labels "${labels[@]}" \
-        --outputpath /work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/ \
+        --outputpath /work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/coupling/ \
         --suffix _EE_quenched_1.5Tc \
-        --leg_pos 1 0.5 \
-        --leg_loc "center right"
+        --leg_pos 1 1 \
+        --leg_loc "upper right" \
+        --fmt
 }
 
 plot_hisq(){
@@ -47,16 +58,16 @@ plot_hisq(){
 
     labels_and_models=(
 #        "LO_piT_1" "${basepath}/SPF_LO_Nf3_0.251_piT_1_hmax.npy"
-        "LO_opt_1" "${basepath}/SPF_LO_Nf3_0.251_opt_1_hmax.npy"
+        "LO_eff_1" "${basepath}/SPF_LO_Nf3_0.251_eff_1_hmax.npy"
 #        "LO_2piT_1" "${basepath}/SPF_LO_Nf3_0.251_2piT_1_hmax.npy"
 #        "NLO_piT_1" "${basepath}/SPF_NLO_Nf3_0.251_piT_1_hmax.npy"
-#        "NLO_opt_1" "${basepath}/SPF_NLO_Nf3_0.251_opt_1_hmax.npy"
+#        "NLO_opt_1" "${basepath}/SPF_NLO_Nf3_0.251_eff_1_hmax.npy"
 #        "NLO_2piT_1" "${basepath}/SPF_NLO_Nf3_0.251_2piT_1_hmax.npy"
 #        "LO_piT_opt" "${basepath}/SPF_LO_Nf3_0.251_piT_opt_hmax.npy"
-#        "LO_opt_opt" "${basepath}/SPF_LO_Nf3_0.251_opt_opt_hmax.npy"
+#        "LO_opt_opt" "${basepath}/SPF_LO_Nf3_0.251_eff_opt_hmax.npy"
 #        "LO_2piT_opt" "${basepath}/SPF_LO_Nf3_0.251_2piT_opt_hmax.npy"
 #        "NLO_piT_opt" "${basepath}/SPF_NLO_Nf3_0.251_piT_opt_hmax.npy"
-        "NLO_opt_opt" "${basepath}/SPF_NLO_Nf3_0.251_opt_opt_hmax.npy"
+        "NLO_eff_opt" "${basepath}/SPF_NLO_Nf3_0.251_eff_opt_hmax.npy"
 #        "NLO_2piT_opt" "${basepath}/SPF_NLO_Nf3_0.251_2piT_opt_hmax.npy"
 
     )
@@ -72,18 +83,18 @@ plot_hisq(){
     done
 
     ../plot_UV_spf.py \
-        --xlims 0.01 1000 \
-        --ylims 0 10 \
+        --xlims 0.1 1000 \
+        --ylims 0 6 \
         --T_in_GeV 0.251 --Nf 3 \
         --PhiUV_files "${models[@]}" \
         --labels "${labels[@]}" \
-        --outputpath /work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/ \
+        --outputpath /work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/coupling/ \
         --suffix _EE_hisq_1.6Tc \
         --leg_pos 1 0.5 \
         --leg_loc "center right"
 
 }
 
-plot_quenched &
-plot_hisq &
+plot_quenched
+#plot_hisq &
 wait
--- spf_reconstruction/plot_fits/plot_UV_spf.py
+++ spf_reconstruction/plot_fits/plot_UV_spf.py
@@ -2,12 +2,9 @@
 import lib_process_data as lpd
 import numpy
 import argparse
-from spf_reconstruction.model_fitting import EE_UV_spf
 
 def parse_args():
     parser = argparse.ArgumentParser()
-
-    EE_UV_spf.add_args(parser)
 
     parser.add_argument('--PhiUV_files', nargs='*')
     parser.add_argument('--labels', nargs='*')
@@ -18,6 +15,7 @@ def parse_args():
     parser.add_argument('--ylims', nargs=2, type=float)
     parser.add_argument('--leg_loc', type=str)
     parser.add_argument('--leg_pos', nargs=2, type=float)
+    parser.add_argument('--fmt', nargs='*')
     args = parser.parse_args()
     return args
 
@@ -26,7 +24,7 @@ def main():
 
     args = parse_args()
 
-    fig, ax, plots = lpd.create_figure(xlabel=r'$\omega/T$', ylabel=r'$\frac{6\pi}{C_F} \frac{\rho}{\omega^3}$', xlims=args.xlims, ylims=args.ylims)
+    fig, ax, plots = lpd.create_figure(xlabel=r'$\omega/T$', ylabel=r'$g^2(\mu)$', xlims=args.xlims, ylims=args.ylims)
 
     # ax.set_yscale('log')
     ax.set_xscale('log')
@@ -36,16 +34,21 @@ def main():
     Nc=3
     Cf=(Nc ** 2 - 1) / 2 / Nc
 
-    for i, file in enumerate(args.PhiUV_files):
-        data = numpy.load(file)
-        xdata = data[:, 0]
-        ydata = data[:, 1]
-        factor = 1/xdata**3 / Cf *6 * numpy.pi
-        ydata *= factor
-        fmt = '--' if int(i)/int(6) < 1 else ':'
-        ax.errorbar(xdata, ydata, fmt=fmt, label=args.labels[i], color=colors[i], alpha=0.9)
+    fmts=['.', ':', '-.', '--', '.', '.', ':', '-.']
 
-    ax.legend(loc=args.leg_loc, bbox_to_anchor=args.leg_pos, handlelength=1, fontsize=6, ncol=2)  #
+    for i, file in enumerate(args.PhiUV_files):
+        if file != "dummy":
+            data = numpy.load(file)
+            xdata = data[:, 0]
+            ydata = data[:, 1]
+            factor = 1/xdata**3 / Cf *6 * numpy.pi
+            # factor = 2 / xdata
+            ydata *= factor
+            ax.errorbar(xdata, ydata, fmt=fmts[i], label=args.labels[i], color=colors[i], alpha=1, zorder=-i)
+        else:
+            ax.errorbar(1, 1, fmt='.', markersize=0, label=args.labels[i])
+
+    ax.legend(loc=args.leg_loc, bbox_to_anchor=args.leg_pos, handlelength=1.4, fontsize=8)#, title=r'$N_f, T/T_c, \mu$')  #
 
     # ax.axvline(x=numpy.pi)
     # ax.axvline(x=2*numpy.pi)
