1111d20c3264c903e3319942252ed20e6bb53866
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-01-19 16:25:46 +0100

    add script to plot comparison with literature

---
+++ spf_reconstruction/plot_fits/example_usage/plot_final_kappas.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+basepath="/work/home/altenkort/work/correlators_flow/data/merged/hisq_ms5_zeuthenFlow/EE/"
+
+plot_hisq(){
+
+    ../plot_final_kappas.py \
+        --input_kappa_files \
+        ${basepath}/T196/EE_kappa_T196.txt \
+        ${basepath}/T220/EE_kappa_T220.txt \
+        ${basepath}/T251/EE_kappa_T251.txt \
+        ${basepath}/T296/EE_kappa_T296.txt \
+        \
+        --labels \
+        196 \
+        220 \
+        251 \
+        296 \
+        --outputpath /work/home/altenkort/work/correlators_flow/plots/hisq_ms5_zeuthenFlow/EE/ \
+        --suffix "hisq" \
+        --temps_in_GeV \
+        0.196 \
+        0.220 \
+        0.251 \
+        0.296 \
+        --Tc_in_GeV 0.180 \
+        --leg_hide \
+        --xlims 0.9 2.5 \
+        --ylims 0 16.5 \
+        --plot_analytical_results --corr EE
+
+}
+
+plot_quenched_EE(){
+
+    basepath="/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/EE/"
+
+    ../plot_final_kappas.py \
+        --input_kappa_files \
+        ${basepath}/EE_kappa_quenched_1.5Tc.txt \
+        --labels \
+        '\textbf{this work} (flow)' \
+        --outputpath "/work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/EE/" \
+        --suffix "EE_quenched_literature" \
+        --temps_in_GeV \
+        0.493 \
+        --Tc_in_GeV 0.315 \
+        --xlims 0.9 3.1 \
+        --ylims 0 7.5 \
+        --plot_EE_quenched_lit --corr EE
+
+}
+
+plot_quenched_BB(){
+
+    basepath="/work/home/altenkort/work/correlators_flow/data/merged/quenched_1.50Tc_zeuthenFlow/BB/"
+
+    ../plot_final_kappas.py \
+        --input_kappa_files \
+        ${basepath}/BB_kappa_quenched_1.5Tc.txt \
+        --labels \
+        '\textbf{this work} (flow)' \
+        --outputpath "/work/home/altenkort/work/correlators_flow/plots/quenched_1.50Tc_zeuthenFlow/BB/" \
+        --suffix "BB_quenched_literature" \
+        --temps_in_GeV \
+        0.482 \
+        --Tc_in_GeV 0.315 \
+        --xlims 0.9 3.1 \
+        --ylims 0 4.5 \
+        --plot_BB_quenched_lit --corr BB
+
+}
+
+plot_hisq &
+plot_quenched_EE &
+plot_quenched_BB &
+
+wait
---
+++ spf_reconstruction/plot_fits/plot_final_kappas.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+import lib_process_data as lpd
+import numpy
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--corr', choices=["EE", "BB"])
+    parser.add_argument('--input_kappa_files', nargs='*')
+    parser.add_argument('--labels', nargs='*')
+    parser.add_argument('--outputpath')
+    parser.add_argument('--suffix')
+    parser.add_argument('--temps_in_GeV', type=float, nargs="*")
+    parser.add_argument('--Tc_in_GeV', type=float, required=True)
+    parser.add_argument('--leg_hide', action="store_true")
+    parser.add_argument('--xlims', nargs=2, type=float)
+    parser.add_argument('--ylims', nargs=2, type=float)
+    parser.add_argument('--plot_EE_quenched_lit', action="store_true")
+    parser.add_argument('--plot_BB_quenched_lit', action="store_true")
+    parser.add_argument('--plot_analytical_results', action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+def load_data(args):
+    data = []
+    for file in args.input_kappa_files:
+        data.append(numpy.loadtxt(file))
+    return data
+
+
+def mean(low, high):
+    return (low+high)/2
+
+
+def err(low, high):
+    return high - (low+high)/2
+
+
+def mean_and_err(low, high):
+    return mean(low,high), err(low,high)
+
+
+def thiscolor(i):
+    return lpd.lighten_color(lpd.get_discrete_color(i), 0.75)
+
+
+def plot_EE_quenched_literature(args, ax, i):
+
+    ms=0
+    myfmt='.'
+
+    #  2015, A. Francis, O. Kaczmarek, M. Laine, T. Neuhaus, and H. Ohno, Phys. Rev. D 92, 116003
+    ax.errorbar(1.44, 2.6, 0.8, color=thiscolor(i), fmt=myfmt, markersize=ms, label=r'Francis \textquotesingle 15 (ML)')
+    i += 1
+
+    #  2020, Nora Brambilla, Viljami Leino, Peter Petreczky, and Antonio Vairo, Phys. Rev. D 102, 074503
+    ax.errorbar(1.1, *mean_and_err(1.91,5.40), markersize=ms, fmt=myfmt, color=thiscolor(i), label=r'TUMQCD \textquotesingle 20 (ML)')
+    ax.errorbar(1.47, *mean_and_err(1.31,3.64), markersize=ms, fmt=myfmt, color=thiscolor(i))
+    ax.errorbar(3,   *mean_and_err(0.63,2.20), markersize=ms, fmt=myfmt, color=thiscolor(i), zorder=-10)
+    i += 1
+
+    #  2022, Nora Brambilla, Viljami Leino, Julian Mayer-Steudte, Peter Petreczky, 	arXiv:2206.02861
+    ax.errorbar(1.5, *mean_and_err(1.70,3.12), markersize=ms, fmt=myfmt, color=thiscolor(i), label=r'TUMQCD \textquotesingle 22 (flow)')
+    i += 1
+
+    # 2022, Debasish Banerjee, Rajiv Gavai, Saumen Datta, Pushan Majumdar, arXiv:2206.15471v1
+    ax.errorbar(1.2, *mean_and_err(2.1,3.5), markersize=ms, fmt=myfmt, color=thiscolor(i), label=r'Banerjee \textquotesingle 22 (ML)')
+    ax.errorbar(1.53, *mean_and_err(1.5,2.8), markersize=ms, fmt=myfmt, color=thiscolor(i))
+    ax.errorbar(2.0, *mean_and_err(1.0,2.3), markersize=ms, fmt=myfmt, color=thiscolor(i))
+    ax.errorbar(2.5, *mean_and_err(0.9,2.1), markersize=ms, fmt=myfmt, color=thiscolor(i))
+    ax.errorbar(3.0, *mean_and_err(0.8,1.8), markersize=ms, fmt=myfmt, color=thiscolor(i))
+    ax.errorbar(3.5, *mean_and_err(0.75, 1.5), markersize=ms, fmt=myfmt, color=thiscolor(i))
+    i += 1
+
+    return i
+
+
+def plot_BB_quenched_literature(args, ax, i):
+    ms = 0
+    myfmt = '.'
+
+    #  2022, D. Banerjee, S. Datta & M. Laine, JHEP08(2022)128
+    ax.errorbar(1.2, *mean_and_err(1,2.6), color=thiscolor(i), fmt=myfmt, markersize=ms, label=r'Banerjee \textquotesingle 22 (ML)')
+    ax.errorbar(1.47, *mean_and_err(1,2.1), color=thiscolor(i), fmt=myfmt, markersize=ms)
+    ax.errorbar(2.0, *mean_and_err(0.6,1.8), color=thiscolor(i), fmt=myfmt, markersize=ms)
+    i += 1
+
+    # 2022, Nora Brambilla, Viljami Leino, Julian Mayer-Steudte, Peter Petreczky, 	arXiv:2206.02861
+    ax.errorbar(1.5, *mean_and_err(1.23,2.74), color=thiscolor(i), fmt=myfmt, markersize=ms, label=r'TUMQCD \textquotesingle 22 (flow*)')
+    i += 1
+
+    return i
+
+
+def plot_analytical_results(args, ax):
+    xlims = ax.get_xlim()
+    ax.errorbar([0, 100], [4 * numpy.pi / 0.9, 4 * numpy.pi / 0.9], alpha=0.5, zorder=-10000, color='grey', fmt=':')
+    ax.errorbar([1.5, 100], [4 * numpy.pi / 6, 4 * numpy.pi / 6], alpha=0.5, zorder=-10000, color='grey', fmt='--')
+    ax.text(xlims[1], 4 * numpy.pi / 0.9, r'AdS/CFT estimate', ha='right', va='bottom')
+    ax.text(xlims[1], 4 * numpy.pi / 6, r'pQCD (NLO, $\alpha\approx 0.5)$', ha='right', va='bottom')
+
+
+def plot_data(args, ax, data, offset, temps):
+    for i, [kappa, kappa_err] in enumerate(data):
+        if i == 0:
+            color = 'k'
+        else:
+            color = lpd.get_discrete_color(offset)
+            offset += 1
+        ax.errorbar(temps[i]/args.Tc_in_GeV, kappa, kappa_err, fmt='.', markersize=0, color=color, label=args.labels[i])
+
+    return offset
+
+
+def do_plot(args, data, temps):
+
+    if args.corr == "EE":
+        kappa_index = r'E'
+    elif args.corr == "BB":
+        kappa_index = r'B'
+
+    fig, ax, plots = lpd.create_figure(xlims=args.xlims, ylims=args.ylims, xlabel=r'$T/T_c$', ylabel=r'$\displaystyle \frac{\kappa_'+kappa_index+r'}{T^3}$')
+
+    offset = 0
+    if args.plot_EE_quenched_lit:
+        offset = plot_EE_quenched_literature(args, ax, offset)
+
+    if args.plot_BB_quenched_lit:
+        offset = plot_BB_quenched_literature(args, ax, offset)
+
+    offset = plot_data(args, ax, data, offset, temps)
+
+    if args.plot_analytical_results:
+        plot_analytical_results(args, ax)
+
+    if not args.leg_hide:
+        ax.legend(**lpd.leg_err_size())
+
+    filename = args.outputpath + "/kappa_"+args.suffix+".pdf"
+    fig.savefig(filename)
+    print("saved correlator plot", filename)
+
+
+def main():
+
+    args = parse_args()
+
+    kappa = load_data(args)
+
+    do_plot(args, kappa, args.temps_in_GeV)
+
+    return
+
+
+if __name__ == '__main__':
+    lpd.print_script_call()
+    main()
+    lpd.save_script_call()
