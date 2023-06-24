188cb309abd9a6de0d815503fb8daa2a998cb7cb
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-06-24 14:43:07 +0200

    fix typo in g2 plot

--- spf_reconstruction/plot_fits/plot_g2.py
+++ spf_reconstruction/plot_fits/plot_g2.py
@@ -24,7 +24,7 @@ def main():
 
     args = parse_args()
 
-    fig, ax, plots = lpd.create_figure(xlabel=r'$\omega/T$', ylabel=r'$g^2(\mu)$', xlims=args.xlims, ylims=args.ylims)
+    fig, ax, plots = lpd.create_figure(xlabel=r'$\mu/T$', ylabel=r'$g^2$', xlims=args.xlims, ylims=args.ylims)
 
     # ax.set_yscale('log')
     ax.set_xscale('log')
