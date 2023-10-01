e20f1d3bb4eb04ac87c0e461fbeb708383af86a7
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-10-01 13:01:00 +0200

    add BB NLO spf and some refactoring

---
+++ spf_reconstruction/model_fitting/BB_UV_functions.py
@@ -0,0 +1,78 @@
+from scipy import integrate
+from scipy.interpolate import CubicSpline
+import rundec
+import numpy as np
+
+
+def nb(x):
+    if x > 200:
+        return 0
+    else:
+        return 1. / (np.exp(x) - 1.)
+
+
+def nf(x):
+    if x > 200:
+        return 0
+    else:
+        return 1. / (np.exp(x) + 1.)
+
+
+def ColorPart(OmegaByT, qByT):
+    X = (qByT ** 2 + 2 * OmegaByT ** 2) * np.log(abs((qByT + OmegaByT) / (qByT - OmegaByT)))
+    Y = qByT * OmegaByT * (np.log(abs(qByT ** 2 - OmegaByT ** 2) / OmegaByT ** 2) + OmegaByT ** 2 / (
+                OmegaByT ** 2 - qByT ** 2) - 2)
+    Z = OmegaByT ** 4 / qByT * (
+                1. / (OmegaByT + qByT) * np.log((OmegaByT + qByT) / OmegaByT) + 1 / (qByT - OmegaByT) * np.log(
+            OmegaByT / abs(qByT - OmegaByT)))
+    return (X + Y + Z) * nb(qByT)
+
+
+def FlavorPart(OmegaByT, qByT):
+    X = (qByT ** 2 + OmegaByT ** 2 / 2) * np.log(abs((qByT + OmegaByT) / (qByT - OmegaByT)))
+    Y = qByT * OmegaByT * (np.log(abs(qByT ** 2 - OmegaByT ** 2) / OmegaByT ** 2) - 1)
+    return (X + Y) * nf(qByT)
+
+
+def get_mu_free(OmegaByT, Nc, Nf):
+    if OmegaByT <= 1e-6:
+        OmegaByT = 1e-6
+    colorPart = \
+    integrate.quad(lambda x: ColorPart(OmegaByT, x) + ColorPart(OmegaByT, 2 * OmegaByT - x), OmegaByT, 2 * OmegaByT,
+                   epsabs=0)[0] + integrate.quad(lambda x: ColorPart(OmegaByT, x), 2 * OmegaByT, 200, epsabs=0)[0]
+    if Nf == 0:
+        return 0 if Nc * colorPart > 0 else Nc * colorPart
+    else:
+        flavorPart = \
+        integrate.quad(lambda x: FlavorPart(OmegaByT, x) + FlavorPart(OmegaByT, 2 * OmegaByT - x), OmegaByT,
+                       2 * OmegaByT, epsabs=0)[0] + \
+        integrate.quad(lambda x: FlavorPart(OmegaByT, x), 2 * OmegaByT, 200, epsabs=0)[0]
+        return 0 if Nc * colorPart + Nf * flavorPart > 0 else Nc * colorPart + Nf * flavorPart
+
+
+def get_cBsq(ir_scale, T_in_GeV, Npoints, gamma_0, Lambda_MSbar, Nf, Nloop):
+    crd = rundec.CRunDec()
+    g2_arr = []
+    for OmegaByT in np.logspace(-6, 3, Npoints, base=10):
+        mu = np.sqrt(4 * (OmegaByT * T_in_GeV) ** 2. + ir_scale ** 2)
+        Alphas = crd.AlphasLam(Lambda_MSbar, mu, Nf, Nloop)
+        g2 = 4. * np.pi * Alphas
+        g2_arr.append([mu, g2])
+    g2_arr = np.array(g2_arr)
+
+    threshold = 1e-10
+    diff = np.empty(len(g2_arr))
+    diff[0] = np.inf  # always retain the 1st element
+    diff[1:] = np.diff(g2_arr[:, 0])
+    mask = diff > threshold
+    new_arr = g2_arr[mask]
+
+    spl = CubicSpline(new_arr[:, 0], new_arr[:, 1], axis=0, bc_type=((2, 0), (2, 0)), extrapolate=True)
+
+    cBsq = []
+    for index in range(len(g2_arr)):
+        this_x = g2_arr[:index, 0]
+        this_y = 2. / np.array(this_x) * (gamma_0 * spl(this_x))
+        integral = integrate.trapz(this_y, this_x)
+        cBsq.append(np.exp(integral))
+    return np.array(cBsq)
--- spf_reconstruction/model_fitting/EE_UV_spf.py
+++ spf_reconstruction/model_fitting/EE_UV_spf.py
@@ -3,6 +3,7 @@ import rundec
 import numpy as np
 import argparse
 import lib_process_data as lpd
+import BB_UV_functions as BB
 
 
 def smooth_max(a, b):
@@ -27,7 +28,7 @@ def add_args(parser):
     return
 
 
-def set_minscale(min_scale, T_in_GeV, Nc, Nf):
+def get_minscale(min_scale, T_in_GeV, Nc, Nf):
 
     effective_theory_thermal_scale = 4 * np.pi * T_in_GeV * np.exp(-np.euler_gamma) * np.exp((-Nc + 4 * Nf * np.log(4)) / (22 * Nc - 4 * Nf))
 
@@ -46,7 +47,7 @@ def set_minscale(min_scale, T_in_GeV, Nc, Nf):
     return min_scale
 
 
-def set_omega_prefactor(omega_prefactor, Nc, Nf, T_in_GeV, min_scale):
+def get_omega_prefactor(omega_prefactor, Nc, Nf, T_in_GeV, min_scale):
 
     mu_opt_omega_term = 2 * np.exp(((24 * np.pi ** 2 - 149) * Nc + 20 * Nf) / (6 * (11 * Nc - 2 * Nf)))  # see eq. 4.17 of arXiv:1006.0867
     omega_exponent = 1
@@ -70,22 +71,14 @@ def set_omega_prefactor(omega_prefactor, Nc, Nf, T_in_GeV, min_scale):
     return omega_prefactor, omega_exponent
 
 
-def get_spf(Nf: int, max_type: str, min_scale, T_in_GeV, omega_prefactor_input, Npoints, Nloop):
-    # check Nf
-    if Nf == 0:
-        Lambda_MSbar = 0.253  # (JHEP11(2017)206)
-    elif Nf == 3:
-        Lambda_MSbar = 0.339  # 2111.09849 (p.11)
-    else:
-        print("ERROR: I don't know any LambdaMSbar for this Nf")
-        exit(1)
+def print_min_scale(scale, min_scale, first, OmegaByT):
+    if scale > min_scale and first:
+        print("scale > min_scale at OmegaByT=", lpd.format_float(OmegaByT))
+        first = False
+    return first
 
-    if max_type == "smooth":
-        maxfunc = smooth_max
-    elif max_type == "hard":
-        maxfunc = np.fmax
 
-    # some constants
+def get_constants(Nf):
     Nc = 3
     r20 = Nc * (149. / 36. - 11. * np.log(2.) / 6. - 2 * np.pi ** 2 / 3.) - Nf * (5. / 9. - np.log(2.) / 3.)
     r21 = (11. * Nc - 2. * Nf) / 12.
@@ -93,43 +86,89 @@ def get_spf(Nf: int, max_type: str, min_scale, T_in_GeV, omega_prefactor_input, 
     b_0 = 11 / (16 * np.pi**2)
     gamma_0 = 3 / (8 * np.pi**2)
 
-    min_scale = set_minscale(min_scale, T_in_GeV, Nc, Nf)
-    omega_prefactor, omega_exponent = set_omega_prefactor(omega_prefactor_input, Nc, Nf, T_in_GeV, min_scale)
+    return Nc, r20, r21, C_F, b_0, gamma_0
+
+
+def get_maxfunc(max_type):
+    if max_type == "smooth":
+        maxfunc = smooth_max
+    elif max_type == "hard":
+        maxfunc = np.fmax
+    return maxfunc
+
+
+def get_lambdamsbar(Nf):
+    if Nf == 0:
+        Lambda_MSbar = 0.253  # (JHEP11(2017)206)
+    elif Nf == 3:
+        Lambda_MSbar = 0.339  # 2111.09849 (p.11)
+    else:
+        print("ERROR: I don't know any LambdaMSbar for this Nf")
+        exit(1)
+    return Lambda_MSbar
+
+
+def get_g2_and_alphas_and_lospf(crd, Lambda_MSbar, mu, Nf, Nloop, C_F, OmegaByT):
+    Alphas = crd.AlphasLam(Lambda_MSbar, mu, Nf, Nloop)
+    g2 = 4. * np.pi * Alphas
+    lo_spf = g2 * C_F * OmegaByT ** 3 / 6. / np.pi
+    return g2, Alphas, lo_spf
+
+
+def inner_loop(index, omega_prefactor, omega_exponent, omega_prefactor_input, OmegaByT, maxfunc, r20, r21, T_in_GeV, min_scale, crd, Lambda_MSbar, Nf, Nc, Nloop, C_F, cBsq):
+    if omega_prefactor_input == "optBB":
+        mu = np.sqrt(4 * (OmegaByT * T_in_GeV) ** 2 + min_scale ** 2)
+        g2, Alphas, lo_spf = get_g2_and_alphas_and_lospf(crd, Lambda_MSbar, mu, Nf, Nloop, C_F, OmegaByT)
+
+        # === NLO ===
+        # The following equations follow C.23 of https://arxiv.org/pdf/2204.14075.pdf
+
+        first_paren = 5./3. * np.log(mu**2/(2*OmegaByT*T_in_GeV)**2) + 134./9. - 2*np.pi**2/3.  # Guy changed the last term: -8*np.pi**2/3 to -2*np.pi**2/3.
+        second_paren = 2./3 * np.log(mu**2/(2*OmegaByT*T_in_GeV)**2) + 26./9.
+        mu_dep_part = lo_spf * (1 + g2 / (4 * np.pi)**2 * (Nc * first_paren - Nf * second_paren))
+        mu_free_part = (g2**2*C_F)/(12*np.pi**3) * BB.get_mu_free(OmegaByT, Nc, Nf)  # from line 3 to the end of eq.(C.23)
+        nlo_spf = cBsq[index] * (mu_dep_part + mu_free_part)
+
+    else:
+        scale = omega_prefactor * (OmegaByT * T_in_GeV) ** omega_exponent
+        mu = maxfunc(min_scale, scale)
+        g2, Alphas, lo_spf = get_g2_and_alphas_and_lospf(crd, Lambda_MSbar, mu, Nf, Nloop, C_F, OmegaByT)
+
+        # === NLO ===
+        # Technically there are two scales here: "mu" and "scale".
+        # "mu" is a scale which is bounded from below (lower bound = thermal scale) and used to evaluate g2.
+        # Above the thermal scale it is given by "scale".
+        # "scale" also occurs in the NLO terms of the spf and is (if omega_prefactor_input=="opt") defined to exactly
+        # cancel the NLO terms (including the log), even below the thermal scale. Therefore, it is not bounded from below.
+        # Effectively, we recover the LO spf with an "NLO scale choice" this way since the NLO terms are always canceled.
+        # However, all of this is a technicality since this distinction only matters at thermal scales where we do not
+        # use the UV spf anyway, but plots are less confusing this way :)
+        l_ = np.log((scale / T_in_GeV) ** 2 / OmegaByT ** 2)
+        nlo_spf = lo_spf * (1 + (r20 + r21 * l_) * Alphas / np.pi)
+
+    return OmegaByT, lo_spf, nlo_spf, g2
+
+
+def get_spf(Nf: int, max_type: str, min_scale, T_in_GeV, omega_prefactor_input, Npoints, Nloop):
+    Lambda_MSbar = get_lambdamsbar(Nf)
+    maxfunc = get_maxfunc(max_type)
+    Nc, r20, r21, C_F, b_0, gamma_0 = get_constants(Nf)
+    min_scale = get_minscale(min_scale, T_in_GeV, Nc, Nf)
+    omega_prefactor, omega_exponent = get_omega_prefactor(omega_prefactor_input, Nc, Nf, T_in_GeV, min_scale)
 
     crd = rundec.CRunDec()
 
-    # calculation
-    OmegaByT_arr = []
-    LO_SPF = []
-    NLO_SPF = []
-    g2_arr = []
-    first = True
-    for OmegaByT in np.logspace(-6, 3, Npoints, base=10):
-        scale = omega_prefactor * (OmegaByT*T_in_GeV)**omega_exponent
-        mu = maxfunc(min_scale, scale)
-        if scale > min_scale and first:
-            print("scale > min_scale at OmegaByT=", lpd.format_float(OmegaByT))
-            first = False
-        Alphas = crd.AlphasLam(Lambda_MSbar, mu, Nf, Nloop)
-        g2 = 4. * np.pi * Alphas
-        g2_arr.append(g2)
-        lo_spf = g2 * C_F * OmegaByT ** 3 / 6. / np.pi
-        l = np.log((scale / T_in_GeV) ** 2 / OmegaByT ** 2)  # TODO should this be scale or mu?
+    cBsq = BB.get_cBsq(min_scale, T_in_GeV, Npoints, gamma_0, Lambda_MSbar, Nf, Nloop)
 
-        if omega_prefactor_input == "optBB":
-            BB_inner_bracket = (b_0-gamma_0) * np.log(scale**2 / (OmegaByT*T_in_GeV)**2) + gamma_0 * np.log(scale**2/min_scale**2)
-            BB_bracket = (1 + g2 * BB_inner_bracket)
-            NLO_SPF.append(lo_spf * BB_bracket)
-        else:
-            NLO_SPF.append(lo_spf * (1 + (r20 + r21 * l) * Alphas / np.pi))
-        OmegaByT_arr.append(OmegaByT)
-        LO_SPF.append(lo_spf)
+    OmegaByT_arr, LO_SPF, NLO_SPF, g2_arr = zip(*[
+        inner_loop(
+            index, omega_prefactor, omega_exponent, omega_prefactor_input, OmegaByT, maxfunc,
+            r20, r21, T_in_GeV, min_scale, crd, Lambda_MSbar, Nf, Nc, Nloop, C_F, cBsq
+        )
+        for index, OmegaByT in enumerate(np.logspace(-6, 3, Npoints, base=10))
+    ])
 
-    OmegaByT_arr = np.asarray(OmegaByT_arr)
-    g2_arr = np.asarray(g2_arr)
-    LO_SPF = np.array(LO_SPF)
-    NLO_SPF = np.array(NLO_SPF)
-    return OmegaByT_arr, g2_arr, LO_SPF, NLO_SPF
+    return np.asarray(OmegaByT_arr), np.asarray(g2_arr), np.asarray(LO_SPF), np.asarray(NLO_SPF)
 
 
 def save_UV_spf(args, OmegaByT_arr, g2_arr, LO_SPF, NLO_SPF):
