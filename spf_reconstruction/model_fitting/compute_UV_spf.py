fix cBsq calculation and use the cBsq(mu), where mu=sqrt(...^2+...^2)

spf_reconstruction/model_fitting/compute_UV_spf.py
@@ -117,29 +117,30 @@ class SpfParams:
 
 def get_cBsq(params: SpfParams):
     # Given input parameters This function returns
-
     # c_B^2(mu,mubarir) =  exp(int^{\mu^2}_{\mubarir^2}\gamma_0 \gsqms(\mubar)\frac{d\mubar^2}{\mubar^2})
-    # one needs g^2 across the range from mu_IR_by_T to mu
+    # Note that in the output function for cBsq, the input variable mu is expected to have units GeV.
 
-    maxfunc = get_maxfunc(params.max_type)
+    mu_IR_in_GeV = params.mu_IR_by_T * params.T_in_GeV
+
+    # Here we just want array that covers the whole range of possible scales in GeV up to the deep UV (we use at most 2*omega and integrate up to 1000T, so 2500 should be enough),
+    # but not unnecessary small values as crd then complains.
+    minimum_mu = mu_IR_in_GeV if mu_IR_in_GeV <= params.min_scale else params.min_scale
+    mu_in_GeV_arr = np.asarray([*np.linspace(0.9*minimum_mu, 2500*params.T_in_GeV, params.Npoints)])
+
     crd = rundec.CRunDec()
 
-    # Get arrays of the scale mu and coupling g^2.
-    mu_in_GeV_arr = np.asarray([maxfunc(params.min_scale, params.omega_prefactor * (OmegaByT * params.T_in_GeV) ** params.omega_exponent)
-                                for OmegaByT in params.OmegaByT_values])
+    def g2(mu_in_GeV):
+        # run g^2 from the MSBAR coupling we found by measuring the flow coupling and converting it at mu_flow/T=mu_MSBAR/T=4
+        mu0 = 4.000425807761551766e+00 * params.T_in_GeV  # TODO remove hard coding of these values and instead read them from the corresponding file
+        alphas_0 = 2.742374650650232670e+00 / (4 * np.pi)
+        return 4. * np.pi * crd.AlphasExact(alphas_0, mu0, mu_in_GeV, params.Nf, params.Nloop)
+    g2_vec = np.vectorize(g2)
 
-    # run g^2 from the MSBAR coupling we found by measuring the flow coupling and converting it at mu_flow/T=mu_MSBAR/T=4
-    mu0 = 4.000425807761551766e+00 * params.T_in_GeV  # TODO remove hard coding of these values and instead read them from the corresponding file
-    alphas_0 = 2.742374650650232670e+00 / (4 * np.pi)
-    g2_arr = np.asarray([4. * np.pi * crd.AlphasExact(alphas_0, mu0, mu, params.Nf, params.Nloop)
-                         for mu in mu_in_GeV_arr])
-
-    integrand_arr = 2 * params.gamma_0 * g2_arr / mu_in_GeV_arr
+    integrand_arr = 2 * params.gamma_0 * g2_vec(mu_in_GeV_arr) / mu_in_GeV_arr
     integrand_spline = interpolate.InterpolatedUnivariateSpline(mu_in_GeV_arr, integrand_arr, k=3, ext=2)
 
-    # Both start_scale and ir_scale are in
-    cBsq_arr = np.asarray(
-        lpd.parallel_function_eval(calc_cBsq, mu_in_GeV_arr, 128, integrand_spline, params.mu_IR_by_T * params.T_in_GeV))
+    cBsq_arr = np.asarray(  # TODO make nproc an input
+        lpd.parallel_function_eval(calc_cBsq, mu_in_GeV_arr, 100, integrand_spline, mu_IR_in_GeV))
 
     cBsq = interpolate.InterpolatedUnivariateSpline(mu_in_GeV_arr, cBsq_arr, k=3, ext=2)
 
@@ -147,7 +148,12 @@ def get_cBsq(params: SpfParams):
 
 
 def calc_cBsq(target_scale, integrand_spline, ir_scale):
-    integral = integrate.quad(integrand_spline, ir_scale, target_scale)[0]
+    # Both start_scale and ir_scale are expected to be in GeV
+    try:
+        integral = integrate.quad(integrand_spline, ir_scale, target_scale)[0]
+    except ValueError:
+        print("error at", target_scale, ir_scale)
+        exit(1)
     return np.exp(integral)
 
 
@@ -172,7 +178,7 @@ def inner_loop(index, params: SpfParams, cBsq):
             mu_dep_part = PhiUVByT3*(1 + g2/(4*np.pi)**2*(params.Nc*first_paren - params.Nf*second_paren))
             mu_free_part = (g2**2*params.C_F)/(12*np.pi**3) * BB.get_mu_free(OmegaByT, params.Nc, params.Nf)  # from line 3 to the end of eq.(C.23)
             # mu_free_part = 0
-            PhiUVByT3 = cBsq(OmegaByT * params.T_in_GeV) * (mu_dep_part + mu_free_part)
+            PhiUVByT3 = cBsq(mu) * (mu_dep_part + mu_free_part)
 
     elif params.corr == "EE":
         g2, Alphas, PhiUVByT3 = get_g2_and_alphas_and_lospf(crd, params.Lambda_MSbar, mu, params.Nf, params.Nloop,
@@ -188,8 +194,7 @@ def inner_loop(index, params: SpfParams, cBsq):
     return OmegaByT, PhiUVByT3, g2
 
 
-def get_parameters(Nf, max_type, min_scale_str, T_in_GeV, omega_prefactor_input, Npoints, Nloop, order, corr,
-                   mu_IR_by_T):
+def get_parameters(Nf, max_type, min_scale_str, T_in_GeV, omega_prefactor_input, Npoints, Nloop, order, corr, mu_IR_by_T_str):
     if order not in ["LO", "NLO"]:
         print("Error: unknown UV spf order", order)
         exit(1)
@@ -200,6 +205,9 @@ def get_parameters(Nf, max_type, min_scale_str, T_in_GeV, omega_prefactor_input,
     min_scale = get_minscale(min_scale_str, T_in_GeV, Nc, Nf)
     omega_prefactor, omega_exponent = get_omega_prefactor(omega_prefactor_input, Nc, Nf, T_in_GeV, min_scale)
     OmegaByT_values = np.logspace(-5, 3, Npoints, base=10)
+
+    mu_IR_by_T = 4 * np.pi * np.exp(1-np.euler_gamma) if mu_IR_by_T_str == "NLO" else (2 * np.pi if mu_IR_by_T_str == "LO" else np.nan)
+    print("mu_IR_by_T=", mu_IR_by_T)
 
     r20 = Nc * (149. / 36. - 11. * np.log(2.) / 6. - 2 * np.pi ** 2 / 3.) - Nf * (5. / 9. - np.log(2.) / 3.)
     r21 = (11. * Nc - 2. * Nf) / 12.
@@ -286,8 +294,7 @@ def add_args(parser):
     parser.add_argument("--corr",
                         help="decide between EE or BB correlator function forms. LO is identical but the scale we use may change.",
                         type=str, choices=["EE", "BB"], required=True)
-    parser.add_argument("--mu_IR_by_T", help="the scale to which the spectral function should be run", type=float,
-                        default=np.nan)
+    parser.add_argument("--mu_IR_by_T", help="In the BB case, this is the scale to which the spectral function should be run", default=np.nan)
 
     return
 
@@ -299,12 +306,12 @@ def main():
     parser.add_argument("--suffix", type=str, help="string to append to end of output file name", default="")
     parser.add_argument("--prefix", type=str, help="string to prepend to end of output file name", default="")
     parser.add_argument("--outputpath", default="/work/home/altenkort/work/correlators_flow/data/merged/spf_coupling/")
+    # parser.add_argument("--nproc", default=1, type=int, help="number of processes")
 
     args = parser.parse_args()
 
     OmegaByT_arr, g2_arr, PhiUVByT3 = get_spf(args.Nf, args.max_type, args.min_scale, args.T_in_GeV,
-                                              args.omega_prefactor, args.Npoints, args.Nloop, args.order, args.corr,
-                                              args.mu_IR_by_T)
+                                              args.omega_prefactor, args.Npoints, args.Nloop, args.order, args.corr, args.mu_IR_by_T)
 
     save_UV_spf(args, OmegaByT_arr, g2_arr, PhiUVByT3)
 