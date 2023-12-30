1b3ab67aa50620816cec523eb6f18c16be7d60db
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2023-12-30 13:05:33 +0100

    adjust fit result kappa BB plot

--- spf_reconstruction/plot_fits/example_usage/plot_fits_quenched.sh
+++ spf_reconstruction/plot_fits/example_usage/plot_fits_quenched.sh
@@ -19,6 +19,7 @@ if [ "${corr}" == "EE" ] ; then
     ylims="1 200"
     add_suffix=""
     xlims="--xlims -0.15 4.15 --xticks 0 1 2 3 4"
+    figsize="9 9"
 
     labels_and_models=(
         '$\text{max}_\text{NLO}$'                   "max_NLO_Nf0_T0.472_min${minscale}_wopt_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
@@ -41,70 +42,67 @@ if [ "${corr}" == "EE" ] ; then
       fi
    done
 
-fi
+elif [ "${corr}" == "BB" ] ; then
 
+    models=()
+    labels=()
 
+      # "23-01-19"
+    nsamples=1000
+    minscale=eff
+    ylims="0.1 5000"
+    add_suffix=""
+    xlims="--xlims -0.15 2    --xticks 0 0.5 1 1.5 2"
+    figsize="7 5"
 
-if [ "${corr}" == "BB" ] ; then
-
-models=()
-labels=()
-
-  # "23-01-19"
-nsamples=1000
-minscale=eff
-ylims="0.1 5000"
-add_suffix=""
-xlims="--xlims -0.15 2    --xticks 0 0.5 1 1.5 2"
-
-input_corr_suffixes=(
-"ref4.0_UVLO_IRNLO"
-"ref4.0_UVLO_IRLO"
-"ref4.0_UVNLO_IRNLO"
-"ref4.0_UVNLO_IRLO"
-)
-
-
-for input_corr_suffix in "${input_corr_suffixes[@]}" ; do
-
-    # 08-27
-    # 10-01
-    # 10-23
-    input_suffix="23-12-16-${input_corr_suffix}"
-
-    # smax_NLO_Nf0_T0.472_minmu_IR_NLO_w2_100smpls_tauTgtr0.24_23-10-23-ref4.0_UVLO_IRNLO
-
-    # Adjusting the minscale based on the suffix
-#    if [[ $input_corr_suffix == *"_IRLO" ]]; then
-#        minscale="2piT" # replace with the desired value
-#    elif [[ $input_corr_suffix == *"_IRNLO" ]]; then
-#        minscale="mu_IR_NLO" # replace with the desired value
-#    fi
-
-    minscale="eff"
-
-    labels_and_models=(                              #max_LO_Nf0_T0.472_mineff_w1_250smpls_tauTgtr0.24_23-07-03
-        '$\text{max}_\text{NLO}$'                   "max_NLO_Nf0_T0.472_min${minscale}_w2_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-        '$\text{max}_\text{LO}$'                    "max_LO_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-        '$\text{smax}_\text{NLO}$'                  "smax_NLO_Nf0_T0.472_min${minscale}_w2_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-                                                   # smax_NLO_Nf0_T0.472_minmu_IR_NLO_w2_100smpls_tauTgtr0.24_23-10-23-ref4.0_UVLO_IRNLO
-        '$\text{smax}_\text{LO}$'                   "smax_LO_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-        '$\text{plaw}_\text{NLO}$'                  "plaw_wIR1.0_wUV6.2832_NLO_Nf0_T0.472_min${minscale}_w2_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-        '$\text{plaw}_\text{LO}$'                   "plaw_wIR1.0_wUV6.2832_LO_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-#            '$\text{trig1}_\text{NLO}$'                 "trig_NLO__beta_1_Nf0_T0.472_min${minscale}_woptBB_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-#            '$\text{trig1}_\text{LO}$'                  "trig_LO__alpha_1_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-#            '$\text{trig2}_\text{NLO}$'                 "trig_NLO__beta_2_Nf0_T0.472_min${minscale}_woptBB_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
-#            '$\text{trig2}_\text{LO}$'                  "trig_LO__alpha_2_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+    input_corr_suffixes=(
+    "ref4.0_UVLO_IRNLO"
+    "ref4.0_UVLO_IRLO"
+    "ref4.0_UVNLO_IRNLO"
+    "ref4.0_UVNLO_IRLO"
     )
 
-  for i in "${!labels_and_models[@]}" ; do
-      if [ $((i % 2)) -eq 0 ] ; then
-          labels+=("${labels_and_models[i]}")
-      else
-          models+=("${labels_and_models[i]}")
-      fi
-  done
-done
+
+    for input_corr_suffix in "${input_corr_suffixes[@]}" ; do
+
+        # 08-27
+        # 10-01
+        # 10-23
+        input_suffix="23-12-16-${input_corr_suffix}"
+
+        # smax_NLO_Nf0_T0.472_minmu_IR_NLO_w2_100smpls_tauTgtr0.24_23-10-23-ref4.0_UVLO_IRNLO
+
+        # Adjusting the minscale based on the suffix
+    #    if [[ $input_corr_suffix == *"_IRLO" ]]; then
+    #        minscale="2piT" # replace with the desired value
+    #    elif [[ $input_corr_suffix == *"_IRNLO" ]]; then
+    #        minscale="mu_IR_NLO" # replace with the desired value
+    #    fi
+
+        minscale="eff"
+
+        labels_and_models=(                              #max_LO_Nf0_T0.472_mineff_w1_250smpls_tauTgtr0.24_23-07-03
+            '$\text{max}_\text{NLO}$'                   "max_NLO_Nf0_T0.472_min${minscale}_w2_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+            '$\text{max}_\text{LO}$'                    "max_LO_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+            '$\text{smax}_\text{NLO}$'                  "smax_NLO_Nf0_T0.472_min${minscale}_w2_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+                                                       # smax_NLO_Nf0_T0.472_minmu_IR_NLO_w2_100smpls_tauTgtr0.24_23-10-23-ref4.0_UVLO_IRNLO
+            '$\text{smax}_\text{LO}$'                   "smax_LO_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+            '$\text{plaw}_\text{NLO}$'                  "plaw_wIR1.0_wUV6.2832_NLO_Nf0_T0.472_min${minscale}_w2_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+            '$\text{plaw}_\text{LO}$'                   "plaw_wIR1.0_wUV6.2832_LO_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+    #            '$\text{trig1}_\text{NLO}$'                 "trig_NLO__beta_1_Nf0_T0.472_min${minscale}_woptBB_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+    #            '$\text{trig1}_\text{LO}$'                  "trig_LO__alpha_1_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+    #            '$\text{trig2}_\text{NLO}$'                 "trig_NLO__beta_2_Nf0_T0.472_min${minscale}_woptBB_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+    #            '$\text{trig2}_\text{LO}$'                  "trig_LO__alpha_2_Nf0_T0.472_min${minscale}_w1_${nsamples}smpls_tauTgtr0.24_${input_suffix}"
+        )
+
+      for i in "${!labels_and_models[@]}" ; do
+          if [ $((i % 2)) -eq 0 ] ; then
+              labels+=("${labels_and_models[i]}")
+          else
+              models+=("${labels_and_models[i]}")
+          fi
+      done
+    done
 
 fi
 
@@ -120,7 +118,7 @@ $xlims \
 --outputpath_data $basepath_work_data/quenched_1.50Tc_zeuthenFlow/${corr}/ \
 --suffix _quenched_1.5Tc${add_suffix} \
 --corr ${corr} \
---figsize 9 9
+--figsize $figsize
 }
 
 plot_fitcorr() {
