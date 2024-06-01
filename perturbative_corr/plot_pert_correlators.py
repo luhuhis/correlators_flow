1b5a76d522a33cb1a22bdeaaa6f2cfdfc94b9458
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2024-06-01 14:30:18 +0200

    update pert flow dep plots

--- perturbative_corr/plot_pert_correlators.py
+++ perturbative_corr/plot_pert_correlators.py
@@ -3,21 +3,16 @@
 import lib_process_data as lpd
 import numpy
 import argparse
+import pandas as pd
 
 
-def main():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--inputfolder', type=str)
-    parser.add_argument('--outputfolder', type=str)
-    parser.add_argument('--Ntau', required=True, type=int)
-    args = parser.parse_args()
-
+def plot_fig_tauT_on_xaxis(args):
     fig, ax, axtwiny = lpd.create_figure(ylabelpos=(0.15, 0.98), ylims=[0.5, 50000], xlims=[0, 0.52], ylabel=r'$\displaystyle \frac{G^\mathrm{LO}}{(g^2 C_F)T^4}$', xlabel=r'$\tau T$')
     ax.set_yscale('log')
     ax.minorticks_off()
     axtwiny.set_yscale('log')
     axtwiny.minorticks_off()
+
 
     # copy these from the corresponding mathematica notebook!
     contCorrResolution = 10
@@ -58,6 +53,60 @@ def main():
     file = args.outputfolder + "/EE_pert_contvslatt_flow.pdf"
     fig.savefig(file)
     print("saved pert corr plot", file)
+
+def plot_fig_flow_on_xaxis(args):
+
+    fig, ax, axtwiny = lpd.create_figure(ylabelpos=(0.17, 0.98), xlabelpos=(0.96,0.01), ylims=[0.5, 50000], xlims=[0, 0.15], ylabel=R'$\displaystyle \frac{G^\mathrm{LO}}{(g^2 C_F)T^4}$', xlabel=R'$\sqrt{8\tau_\mathrm{F}} T$', ylabelbox=None)
+    ax.set_yscale('log')
+    ax.minorticks_off()
+    axtwiny.set_yscale('log')
+    axtwiny.minorticks_off()
+
+    plots = []
+    zorder = 0
+    plot_tauTs = ["0.041667", "0.125", "0.25", "0.375", "0.5"]
+    colors = [f"C{i}" for i in range(len(plot_tauTs))]
+
+    for prefix, fmt, subtitle in zip(["EE_cont_flow_", "EE_latt_flow_"], ["-", "--"], ["cont.", "latt."]):
+
+        plots.append(ax.errorbar(0,0, label=subtitle, ms=0, lw=0))
+
+        # Load the data from the text file
+        file_path = f"{args.inputfolder}{prefix}{args.Ntau}.dat"
+        data = pd.read_csv(file_path, delim_whitespace=True, header=None, names=['x', 'y', 'value'])
+
+        # Pivot the data to get it in the right format for plotting
+        pivot_data = data.pivot(index='x', columns='y', values='value')
+
+        i = 0
+        for key in pivot_data.index:
+            if str(key) in plot_tauTs:
+                plots.append(ax.errorbar(pivot_data.columns, pivot_data.loc[key], label=f'{round(key,3)}', fmt=fmt, zorder=zorder, color=colors[i]))
+                zorder -= 1
+                i += 1
+        
+
+
+    ax.legend(handles=plots, title=R'$\tau T$', loc="upper right", frameon=False, edgecolor='none', fancybox=False, facecolor="w", labelspacing=0.1, borderpad=0.1,
+                       handletextpad=0.4, handlelength=1, ncol=2, columnspacing=0.5)
+    lpd.create_folder(args.outputfolder)
+    file = args.outputfolder + "/EE_pert_contvslatt_flow_dep.pdf"
+    fig.savefig(file)
+    print("saved pert corr plot", file)
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inputfolder', type=str)
+    parser.add_argument('--outputfolder', type=str)
+    parser.add_argument('--Ntau', required=True, type=int)
+    # parser.add_argument('--swap_xaxis_and_leg', action="store_true", type=bool, help="Put flow time on the x-axis and tauT in the legend.")
+    args = parser.parse_args()
+
+    # plot_fig_tauT_on_xaxis(args)
+    plot_fig_flow_on_xaxis(args)
+
 
     return
 
